9a8bba5ce81eff74b7b6502fb40a53a7202bb086
Luís Egídio <luis.egidio@gmail.com>
2015-07-22 18:56:28 +0100

    * [WIP] New Feature: enforcing MaximumNumberOfCreditsForEnrolmentPeriod curricular rules' values based on configuration and/or current installation values * Minor Change: logging currently used GradeScaleLogic

--- src/main/java/org/fenixedu/ulisboa/specifications/ULisboaConfiguration.java
+++ src/main/java/org/fenixedu/ulisboa/specifications/ULisboaConfiguration.java
@@ -15,6 +15,18 @@ public class ULisboaConfiguration {
         @ConfigurationProperty(key = "gradescale.typequalitative.logic.class")
         public String typeQualitativeGradeScaleLogic();
 
+        @ConfigurationProperty(key = "domain.academic.enrolments.maxCredits.value")
+        public Double getEnrolmentsMaxCredits();
+
+        @ConfigurationProperty(key = "domain.academic.enrolments.maxCredits.valuePartialTime")
+        public Double getEnrolmentsMaxCreditsPartialTime();
+
+        @ConfigurationProperty(key = "domain.academic.enrolments.maxCredits.updateValuesFromConfiguration", defaultValue = "false")
+        public Boolean getEnrolmentsMaxCreditsUpdateFromConfiguration();
+        
+        @ConfigurationProperty(key = "domain.academic.enrolments.maxCredits.updateRulesValues", defaultValue = "false")
+        public Boolean getEnrolmentsMaxCreditsUpdateRules();
+        
         @ConfigurationProperty(key = "quality.mode")
         public Boolean isQualityMode();
 
--- src/main/java/org/fenixedu/ulisboa/specifications/servlet/FenixeduUlisboaSpecificationsInitializer.java
+++ src/main/java/org/fenixedu/ulisboa/specifications/servlet/FenixeduUlisboaSpecificationsInitializer.java
@@ -10,7 +10,9 @@ import org.fenixedu.academic.domain.GradeScale.GradeScaleLogic;
 import org.fenixedu.bennu.core.util.CoreConfiguration;
 import org.fenixedu.bennu.portal.domain.PortalConfiguration;
 import org.fenixedu.ulisboa.specifications.ULisboaConfiguration;
+import org.fenixedu.ulisboa.specifications.domain.MaximumNumberOfCreditsForEnrolmentPeriodEnforcer;
 import org.fenixedu.ulisboa.specifications.domain.ULisboaPortalConfiguration;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import pt.ist.fenixframework.Atomic;
@@ -19,24 +21,21 @@ import pt.ist.fenixframework.Atomic.TxMode;
 @WebListener
 public class FenixeduUlisboaSpecificationsInitializer implements ServletContextListener {
 
+    private static final Logger logger = LoggerFactory.getLogger(FenixeduUlisboaSpecificationsInitializer.class);
+
+    @Override
+    public void contextDestroyed(ServletContextEvent event) {
+    }
+
     @Atomic(mode = TxMode.SPECULATIVE_READ)
     @Override
     public void contextInitialized(ServletContextEvent event) {
         configurePortal();
-        try {
-            configureType20GradeScaleLogic();
-            configureTypeQualitativeGradeScaleLogic();
-        } catch (RuntimeException e) {
-            if (CoreConfiguration.getConfiguration().developmentMode()) {
-                LoggerFactory.getLogger(getClass()).info(
-                        "You do not have a configured degree scale. This may lead to wrong system behaviour");
-                return;
-            }
-            throw e;
-        }
+        configureGradeScaleLogics();
+        configureMaximumNumberOfCreditsForEnrolmentPeriod();
     }
 
-    protected void configurePortal() {
+    static private void configurePortal() {
         ULisboaPortalConfiguration ulisboaPortal = PortalConfiguration.getInstance().getUlisboaPortal();
         if (ulisboaPortal == null) {
             ulisboaPortal = new ULisboaPortalConfiguration();
@@ -44,35 +43,75 @@ public class FenixeduUlisboaSpecificationsInitializer implements ServletContextL
         }
     }
 
-    protected void configureTypeQualitativeGradeScaleLogic() {
-        if (StringUtils.isBlank(ULisboaConfiguration.getConfiguration().typeQualitativeGradeScaleLogic())) {
-            throw new RuntimeException("Property 'gradescale.typequalitative.logic.class' must be defined in configuration file");
-
+    static private void configureGradeScaleLogics() {
+        try {
+            configureType20GradeScaleLogic();
+            configureTypeQualitativeGradeScaleLogic();
+        } catch (RuntimeException e) {
+            if (CoreConfiguration.getConfiguration().developmentMode()) {
+                logger.info("You do not have a configured degree scale. This may lead to wrong system behaviour");
+                return;
+            }
+            throw e;
         }
-
-        GradeScale.TYPEQUALITATIVE.setLogic(loadGradeScaleLogicClass(ULisboaConfiguration.getConfiguration()
-                .typeQualitativeGradeScaleLogic()));
     }
 
-    protected void configureType20GradeScaleLogic() {
+    static private void configureTypeQualitativeGradeScaleLogic() {
+        if (StringUtils.isBlank(ULisboaConfiguration.getConfiguration().typeQualitativeGradeScaleLogic())) {
+            throw new RuntimeException("Property 'gradescale.typequalitative.logic.class' must be defined in configuration file");
+        }
+
+        final GradeScaleLogic logic = loadClass(ULisboaConfiguration.getConfiguration().typeQualitativeGradeScaleLogic());
+        if (logic != null) {
+            logger.info("Using " + logic.getClass().getSimpleName());
+        }
+        
+        GradeScale.TYPEQUALITATIVE.setLogic(logic);
+    }
+
+    static private void configureType20GradeScaleLogic() {
         if (StringUtils.isBlank(ULisboaConfiguration.getConfiguration().type20GradeScaleLogic())) {
             throw new RuntimeException("Property 'gradescale.type20.logic.class' must be defined in configuration file");
         }
 
-        GradeScale.TYPE20.setLogic(loadGradeScaleLogicClass(ULisboaConfiguration.getConfiguration().type20GradeScaleLogic()));
-    }
-
-    private GradeScaleLogic loadGradeScaleLogicClass(String className) {
-
-        try {
-            return (GradeScaleLogic) Class.forName(className).newInstance();
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-            throw new RuntimeException("An error occured loading grade scale logic class :" + className);
+        final GradeScaleLogic logic = loadClass(ULisboaConfiguration.getConfiguration().type20GradeScaleLogic());
+        if (logic != null) {
+            logger.info("Using " + logic.getClass().getSimpleName());
         }
-
+        
+        GradeScale.TYPE20.setLogic(logic);
     }
 
-    @Override
-    public void contextDestroyed(ServletContextEvent event) {
+    @SuppressWarnings("unchecked")
+    static private <T> T loadClass(final String className) {
+        try {
+
+            T result = null;
+            if (StringUtils.isNotBlank(className)) {
+                result = (T) Class.forName(className).newInstance();
+            }
+            return result;
+
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException("An error occured loading class: " + className, e);
+        }
     }
+
+    static private void configureMaximumNumberOfCreditsForEnrolmentPeriod() {
+        MaximumNumberOfCreditsForEnrolmentPeriodEnforcer.init();
+
+        final MaximumNumberOfCreditsForEnrolmentPeriodEnforcer enforcer =
+                MaximumNumberOfCreditsForEnrolmentPeriodEnforcer.getInstance();
+
+        if (enforcer == null) {
+
+            LoggerFactory.getLogger(MaximumNumberOfCreditsForEnrolmentPeriodEnforcer.class).warn(
+                    "Unavailable. If unchanged, rules are using default FenixEdu Academic's values.");
+
+        } else if (ULisboaConfiguration.getConfiguration().getEnrolmentsMaxCreditsUpdateRules()) {
+
+            enforcer.updateRules();
+        }
+    }
+
 }
