cee09e2301e175c45309fadb165b9d19a018f689
nuno.pinheiro <nuno.pinheiro@qub-it.com>
2015-07-23 18:34:21 +0100

    First iteration of data export to moodle

---
+++ src/main/java/org/fenixedu/ulisboa/specifications/ui/ff/moodleexport/MoodleExportController.java
@@ -0,0 +1,222 @@
+/**
+ * This file was created by Quorum Born IT <http://www.qub-it.com/> and its 
+ * copyright terms are bind to the legal agreement regulating the FenixEdu@ULisboa 
+ * software development project between Quorum Born IT and Serviços Partilhados da
+ * Universidade de Lisboa:
+ *  - Copyright © 2015 Quorum Born IT (until any Go-Live phase)
+ *  - Copyright © 2015 Universidade de Lisboa (after any Go-Live phase)
+ *
+ * Contributors: nuno.pinheiro@qub-it.com
+ *
+ * 
+ * This file is part of FenixEdu fenixedu-ulisboa-specifications.
+ *
+ * FenixEdu Specifications is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FenixEdu Specifications is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with FenixEdu Specifications.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.fenixedu.ulisboa.specifications.ui.ff.moodleexport;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.fenixedu.academic.domain.ExecutionYear;
+import org.fenixedu.academic.domain.Person;
+import org.fenixedu.academic.domain.Teacher;
+import org.fenixedu.academic.domain.student.Student;
+import org.fenixedu.bennu.spring.portal.SpringFunctionality;
+import org.fenixedu.ulisboa.specifications.ui.FenixEduUlisboaSpecificationsBaseController;
+import org.fenixedu.ulisboa.specifications.ui.FenixeduUlisboaSpecificationsController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(MoodleExportController.CONTROLLER_URL)
+@SpringFunctionality(app = FenixeduUlisboaSpecificationsController.class, title = "label.title.moodleExport",
+        accessGroup = "logged")
+public class MoodleExportController extends FenixEduUlisboaSpecificationsBaseController {
+
+    public static final String CONTROLLER_URL = "/ff/moodleexport";
+
+    @RequestMapping
+    public String home(Model model) {
+        return "ff/moodleExport/homescreen";
+    }
+
+    private MoodleExportBean getMoodleExportBean(Model model) {
+        return (MoodleExportBean) model.asMap().get("MoodleExportBean");
+    }
+
+    private void setMoodleExportBean(MoodleExportBean MoodleExportBean, Model model) {
+        model.addAttribute("MoodleExportBean", MoodleExportBean);
+    }
+
+    private static final String _LISTSTUDENTS_URI = "/liststudents";
+    public static final String LISTSTUDENTS_URL = CONTROLLER_URL + _LISTSTUDENTS_URI;
+
+    @RequestMapping(value = _LISTSTUDENTS_URI)
+    public String listStudents(Model model) {
+        List<MoodleExportBean> liststudentsResultsDataSet =
+                getSearchUniverseListStudentsDataSet().map(student -> populateStudents(student))
+                        .filter(meb -> meb.getCourses().size() > 0).collect(Collectors.toList());
+
+        //add the results dataSet to the model
+        model.addAttribute("resultsDataSet", liststudentsResultsDataSet);
+
+        int maxCourses = liststudentsResultsDataSet.stream().mapToInt(mb -> mb.getCourses().size()).max().orElse(0);
+        model.addAttribute("numberOfCourseColumns", maxCourses);
+
+        model.addAttribute("listType", "Alunos");
+        return "ff/moodleExport/list";
+    }
+
+    @RequestMapping(value = _LISTTEACHERS_URI)
+    public String listTeachers(Model model) {
+        List<MoodleExportBean> listteachersResultsDataSet =
+                getSearchUniverseListTeachersDataSet().map(p -> populateProfessors(p)).filter(meb -> meb.getCourses().size() > 0)
+                        .collect(Collectors.toList());
+
+        //add the results dataSet to the model
+        model.addAttribute("resultsDataSet", listteachersResultsDataSet);
+        //add the max number of courses
+        int maxCourses = listteachersResultsDataSet.stream().mapToInt(mb -> mb.getCourses().size()).max().orElse(0);
+        model.addAttribute("numberOfCourseColumns", maxCourses);
+
+        model.addAttribute("listType", "Docentes");
+        return "ff/moodleExport/list";
+    }
+
+    private MoodleExportBean populateStudents(Student student) {
+        MoodleExportBean moodleExportBean = new MoodleExportBean();
+        populatePerson(student.getPerson(), moodleExportBean);
+        moodleExportBean.setRole1("student");
+        ExecutionYear currentExecutionYear = ExecutionYear.readCurrentExecutionYear();
+        String yearName = calculateYearName(currentExecutionYear);
+        List<String> coursesNames =
+                student.getActiveRegistrations().stream().flatMap(r -> r.getEnrolments(currentExecutionYear).stream())
+                        .flatMap(en -> en.getExecutionCourses().stream()).map(ec -> ec.getName() + " " + yearName)
+                        .collect(Collectors.toList());
+        moodleExportBean.setCourses(coursesNames);
+        return moodleExportBean;
+    }
+
+    private void populatePerson(Person person, MoodleExportBean moodleExportBean) {
+        String firstAndLastName = person.getFirstAndLastName();
+        String firstName = firstAndLastName.split(" ")[0];
+        String lastName = firstAndLastName.split(" ")[1];
+        moodleExportBean.setFirstname(firstName);
+        moodleExportBean.setLastname(lastName);
+        moodleExportBean.setEmail(person.getInstitutionalEmailAddressValue());
+        moodleExportBean.setUsername(person.getUsername());
+        moodleExportBean.setAuth("shibboleth");
+    }
+
+    private MoodleExportBean populateProfessors(Teacher teacher) {
+        MoodleExportBean moodleExportBean = new MoodleExportBean();
+        populatePerson(teacher.getPerson(), moodleExportBean);
+        moodleExportBean.setRole1("teacher");
+        ExecutionYear currentExecutionYear = ExecutionYear.readCurrentExecutionYear();
+        String yearName = calculateYearName(currentExecutionYear);
+        List<String> coursesNames =
+                teacher.getProfessorships(currentExecutionYear).stream()
+                        .map(p -> p.getExecutionCourse().getName() + " " + yearName).collect(Collectors.toList());
+        moodleExportBean.setCourses(coursesNames);
+        return moodleExportBean;
+    }
+
+    private String calculateYearName(ExecutionYear currentExecutionYear) {
+        //Transforms fenix execution year format 20XX/20YY to the required format [XX-YY]
+        String yearName = "[";
+        yearName += currentExecutionYear.getName().substring(2, 4);
+        yearName += "-";
+        yearName += currentExecutionYear.getName().substring(7, 9);
+        yearName += "]";
+        return yearName;
+    }
+
+    private Stream<Student> getSearchUniverseListStudentsDataSet() {
+        return ExecutionYear.readCurrentExecutionYear().getExecutionPeriodsSet().stream()
+                .flatMap(es -> es.getEnrolmentsSet().stream()).map(en -> en.getStudent()).distinct();
+    }
+
+//				
+    private static final String _LISTTEACHERS_URI = "/listteachers";
+    public static final String LISTTEACHERS_URL = CONTROLLER_URL + _LISTTEACHERS_URI;
+
+    private Stream<Teacher> getSearchUniverseListTeachersDataSet() {
+        return ExecutionYear.readCurrentExecutionYear().getExecutionPeriodsSet().stream()
+                .flatMap(es -> es.getAssociatedExecutionCoursesSet().stream()).flatMap(ec -> ec.getProfessorshipsSet().stream())
+                .map(p -> p.getTeacher()).distinct();
+    }
+
+    public static class MoodleExportBean {
+        String username, firstname, lastname, email, role1, auth;
+        Collection<String> courses;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getFirstname() {
+            return firstname;
+        }
+
+        public void setFirstname(String firstname) {
+            this.firstname = firstname;
+        }
+
+        public String getLastname() {
+            return lastname;
+        }
+
+        public void setLastname(String lastname) {
+            this.lastname = lastname;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getRole1() {
+            return role1;
+        }
+
+        public void setRole1(String role1) {
+            this.role1 = role1;
+        }
+
+        public String getAuth() {
+            return auth;
+        }
+
+        public void setAuth(String auth) {
+            this.auth = auth;
+        }
+
+        public Collection<String> getCourses() {
+            return courses;
+        }
+
+        public void setCourses(Collection<String> courses) {
+            this.courses = courses;
+        }
+    }
+}
---
+++ src/main/webapp/WEB-INF/ff/moodleExport/list.jsp
@@ -0,0 +1,164 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jstl/fmt" %>
+<spring:url var="datatablesUrl" value="/javaScript/dataTables/media/js/jquery.dataTables.latest.min.js"/>
+<spring:url var="datatablesBootstrapJsUrl" value="/javaScript/dataTables/media/js/jquery.dataTables.bootstrap.min.js"></spring:url>
+<script type="text/javascript" src="${datatablesUrl}"></script>
+<script type="text/javascript" src="${datatablesBootstrapJsUrl}"></script>
+<spring:url var="datatablesCssUrl" value="/CSS/dataTables/dataTables.bootstrap.min.css"/>
+
+<link rel="stylesheet" href="${datatablesCssUrl}"/>
+<spring:url var="datatablesI18NUrl" value="/javaScript/dataTables/media/i18n/${portal.locale.language}.json"/>
+<link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/CSS/dataTables/dataTables.bootstrap.min.css"/>
+
+${portal.toolkit()}
+
+<link href="${pageContext.request.contextPath}/webjars/datatables-tools/2.2.4/css/dataTables.tableTools.css" rel="stylesheet"/>
+<script src="${pageContext.request.contextPath}/webjars/datatables-tools/2.2.4/js/dataTables.tableTools.js"></script>
+<link href="${pageContext.request.contextPath}/webjars/select2/4.0.0-rc.2/dist/css/select2.min.css" rel="stylesheet" />
+<script src="${pageContext.request.contextPath}/webjars/select2/4.0.0-rc.2/dist/js/select2.min.js"></script>						
+<script type="text/javascript" src="${pageContext.request.contextPath}/webjars/bootbox/4.4.0/bootbox.js" ></script>
+
+
+
+<%-- TITLE --%>
+<div class="page-header">
+	<h1><spring:message code="label.moodleExport.list" /> ${listType}
+		<small></small>
+	</h1>
+</div>
+<%-- NAVIGATION --%>
+<div class="well well-sm" style="display:inline-block">
+	<span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span>&nbsp;<a class="" href="${pageContext.request.contextPath}/ff/moodleexport"   ><spring:message code="label.event.back"/></a>
+</div>
+	<c:if test="${not empty infoMessages}">
+				<div class="alert alert-info" role="alert">
+					
+					<c:forEach items="${infoMessages}" var="message"> 
+						<p> <span class="glyphicon glyphicon glyphicon-ok-sign" aria-hidden="true">&nbsp;</span>
+  							${message}
+  						</p>
+					</c:forEach>
+					
+				</div>	
+			</c:if>
+			<c:if test="${not empty warningMessages}">
+				<div class="alert alert-warning" role="alert">
+					
+					<c:forEach items="${warningMessages}" var="message"> 
+						<p> <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true">&nbsp;</span>
+  							${message}
+  						</p>
+					</c:forEach>
+					
+				</div>	
+			</c:if>
+			<c:if test="${not empty errorMessages}">
+				<div class="alert alert-danger" role="alert">
+					
+					<c:forEach items="${errorMessages}" var="message"> 
+						<p> <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true">&nbsp;</span>
+  							${message}
+  						</p>
+					</c:forEach>
+					
+				</div>	
+			</c:if>
+
+
+
+
+
+<c:choose>
+	<c:when test="${not empty resultsDataSet}">
+		<table id="listTable" class="table responsive table-bordered table-hover">
+			<thead>
+				<tr>
+					<%--!!!  Field names here --%>
+					<th>username</th>
+					 <th>firstname</th>
+					 <th>lastname</th>
+					 <th>email</th>
+
+					 <c:forEach var="i" begin="0" end="${numberOfCourseColumns}">
+						<th>course${i}</th>					
+			         </c:forEach>
+					 
+					 <th>role1</th>
+					 <th>auth</th>
+				</tr>
+			</thead>
+			<tbody>
+				
+			</tbody>
+		</table>
+	</c:when>
+	<c:otherwise>
+				<div class="alert alert-warning" role="alert">
+					
+					<p> <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true">&nbsp;</span>			<spring:message code="label.noResultsFound" /></p>
+					
+				</div>	
+		
+	</c:otherwise>
+</c:choose>
+
+<script>
+	var listDataSet = [
+	                           
+			<c:forEach items="${resultsDataSet}" var="searchResult">
+				<%-- Field access / formatting  here CHANGE_ME --%>
+				{
+				"DT_RowId" : '<c:out value='${searchResult.username}'/>',
+					"username" 	: "<c:out value='${searchResult.username}'/>",
+ 					"firstname"	: "<c:out value='${searchResult.firstname}'/>",
+ 					"lastname" 	: "<c:out value='${searchResult.lastname}'/>",
+ 					"email"		: "<c:out value='${searchResult.email}'/>",
+ 					"role1"		: "<c:out value='${searchResult.role1}'/>",
+ 					"auth"		: "<c:out value='${searchResult.auth}'/>",
+					<!-- Iterate courses-->
+					<c:set var="count" value="0" scope="page" />
+					<c:forEach items="${searchResult.courses}" var="course">
+						"course${count}" : 	"${course}",
+						<c:set var="count" value="${count + 1}" scope="page"/>
+					</c:forEach>
+					<c:forEach var="i" begin="${count}" end="${numberOfCourseColumns}">
+						"course${i}" : 	"",					
+	         		</c:forEach>
+				
+			},
+            </c:forEach>
+    ];
+	
+	$(document).ready(function() {
+
+		var table = $('#listTable').DataTable({language : {
+			url : "${datatablesI18NUrl}",			
+		},
+		"columns": [
+			{ data: 'username' },
+			{ data: 'firstname' },
+			{ data: 'lastname' },
+			{ data: 'email' },
+			<c:forEach var="i" begin="0" end="${numberOfCourseColumns}">
+				{ data: 'course${i}' },					
+         	</c:forEach>
+			{ data: 'role1' },
+			{ data: 'auth' },
+			
+		],
+		"data" : listDataSet,
+"dom": '<"col-sm-6"l><"col-sm-3"f><"col-sm-3"T>rtip', 
+        "tableTools": {
+            "sSwfPath": "${pageContext.request.contextPath}/webjars/datatables-tools/2.2.4/swf/copy_csv_xls_pdf.swf"        	
+        }
+		});
+		table.columns.adjust().draw();
+		
+		  $('#listTable tbody').on( 'click', 'tr', function () {
+		        $(this).toggleClass('selected');
+		    } );
+		  
+	}); 
+</script>
+
