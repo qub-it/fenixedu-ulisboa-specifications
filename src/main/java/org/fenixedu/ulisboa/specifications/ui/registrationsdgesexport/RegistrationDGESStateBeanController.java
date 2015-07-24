6163ab28a59d224566a3c5e47a778b66b992c750
nuno.pinheiro <nuno.pinheiro@qub-it.com>
2015-07-24 19:23:06 +0100

    New feature: Export registration status of students imported from dges

---
+++ src/main/java/org/fenixedu/ulisboa/specifications/ui/registrationsdgesexport/RegistrationDGESStateBeanController.java
@@ -0,0 +1,141 @@
+/**
+ * This file was created by Quorum Born IT <http://www.qub-it.com/> and its 
+ * copyright terms are bind to the legal agreement regulating the FenixEdu@ULisboa 
+ * software development project between Quorum Born IT and Serviços Partilhados da
+ * Universidade de Lisboa:
+ *  - Copyright © 2015 Quorum Born IT (until any Go-Live phase)
+ *  - Copyright © 2015 Universidade de Lisboa (after any Go-Live phase)
+ *
+ * Contributors: nuno.pinheiro@qub-it.com
+ *
+ * 
+ * This file is part of FenixEdu ULisboaSpecifications.
+ *
+ * FenixEdu Specifications is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FenixEdu Specifications is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with FenixEdu Specifications.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.fenixedu.ulisboa.specifications.ui.registrationsdgesexport;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.fenixedu.academic.domain.EntryPhase;
+import org.fenixedu.academic.domain.ExecutionYear;
+import org.fenixedu.academic.domain.candidacy.StudentCandidacy;
+import org.fenixedu.bennu.core.domain.Bennu;
+import org.fenixedu.bennu.spring.portal.SpringFunctionality;
+import org.fenixedu.ulisboa.specifications.ui.FenixEduUlisboaSpecificationsBaseController;
+import org.fenixedu.ulisboa.specifications.ui.FenixeduUlisboaSpecificationsController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringFunctionality(app = FenixeduUlisboaSpecificationsController.class, title = "label.title.registrationsDgesExport",
+        accessGroup = "logged")
+@RequestMapping(RegistrationDGESStateBeanController.CONTROLLER_URL)
+public class RegistrationDGESStateBeanController extends FenixEduUlisboaSpecificationsBaseController {
+
+    public static final String CONTROLLER_URL = "/registrationsdgesexport";
+
+    private static final String _SEARCH_URI = "/";
+    public static final String SEARCH_URL = CONTROLLER_URL + _SEARCH_URI;
+
+    @RequestMapping
+    public String search(Model model, @RequestParam(required = false) ExecutionYear executionYear,
+            @RequestParam(required = false) Integer phase) {
+        executionYear = executionYear != null ? executionYear : ExecutionYear.readCurrentExecutionYear();
+        phase = phase != null ? phase : 1;
+        List<RegistrationDGESStateBean> searchregistrationdgesstatebeanResultsDataSet =
+                filterSearchRegistrationDGESStateBean(executionYear, phase);
+
+        model.addAttribute("searchregistrationdgesstatebeanResultsDataSet", searchregistrationdgesstatebeanResultsDataSet);
+        model.addAttribute("executionYears",
+                Bennu.getInstance().getExecutionYearsSet().stream().sorted().collect(Collectors.toList()));
+        model.addAttribute("phases", getPhases());
+        model.addAttribute("selectedExecutionYear", executionYear);
+        model.addAttribute("selectedPhase", phase);
+        return "registrationsdgesexport/search";
+    }
+
+    private Collection<Integer> getPhases() {
+        EntryPhase[] values = EntryPhase.values();
+        List<Integer> phases = new ArrayList<>();
+        for (EntryPhase phase : values) {
+            phases.add(phase.getPhaseNumber());
+        }
+        return phases;
+    }
+
+    private List<RegistrationDGESStateBean> filterSearchRegistrationDGESStateBean(ExecutionYear executionYear, int phase) {
+        Predicate<? super StudentCandidacy> hasDgesImportationForCurrentPhase =
+                sc -> sc.getDgesStudentImportationProcess() != null
+                        && sc.getDgesStudentImportationProcess().getEntryPhase().getPhaseNumber() == phase;
+        return executionYear.getStudentCandidacies().stream().filter(hasDgesImportationForCurrentPhase)
+                .map(sc -> populateBean(sc)).collect(Collectors.toList());
+    }
+
+    private RegistrationDGESStateBean populateBean(StudentCandidacy studentCandidacy) {
+        String documentIdNumber = studentCandidacy.getPerson().getDocumentIdNumber();
+        String name = studentCandidacy.getPerson().getName();
+        String registrationStatus = "";
+        if (studentCandidacy.getRegistration() != null && !studentCandidacy.getRegistration().isCanceled()) {
+            registrationStatus = "Sim";
+        } else {
+            registrationStatus = "Não";
+        }
+        return new RegistrationDGESStateBean(documentIdNumber, name, registrationStatus);
+    }
+
+    public static class RegistrationDGESStateBean {
+        String idNumber;
+
+        String name;
+        String registrationState;
+
+        public RegistrationDGESStateBean(String idNumber, String name, String registrationState) {
+            super();
+            this.idNumber = idNumber;
+            this.name = name;
+            this.registrationState = registrationState;
+        }
+
+        public String getIdNumber() {
+            return idNumber;
+        }
+
+        public void setIdNumber(String idNumber) {
+            this.idNumber = idNumber;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getRegistrationState() {
+            return registrationState;
+        }
+
+        public void setRegistrationState(String registrationState) {
+            this.registrationState = registrationState;
+        }
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/registrationsdgesexport/search.jsp
@@ -0,0 +1,163 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jstl/fmt" %>
+<spring:url var="datatablesUrl" value="/javaScript/dataTables/media/js/jquery.dataTables.latest.min.js"/>
+<spring:url var="datatablesBootstrapJsUrl" value="/javaScript/dataTables/media/js/jquery.dataTables.bootstrap.min.js"></spring:url>
+<script type="text/javascript" src="${datatablesUrl}"></script>
+<script type="text/javascript" src="${datatablesBootstrapJsUrl}"></script>
+<spring:url var="datatablesCssUrl" value="/CSS/dataTables/dataTables.bootstrap.min.css"/>
+
+<link rel="stylesheet" href="${datatablesCssUrl}"/>
+<spring:url var="datatablesI18NUrl" value="/javaScript/dataTables/media/i18n/${portal.locale.language}.json"/>
+<link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/CSS/dataTables/dataTables.bootstrap.min.css"/>
+
+${portal.toolkit()}
+
+<link href="${pageContext.request.contextPath}/static/specifications/css/dataTables.responsive.css" rel="stylesheet"/>
+<script src="${pageContext.request.contextPath}/static/specifications/js/dataTables.responsive.js"></script>
+<link href="${pageContext.request.contextPath}/webjars/datatables-tools/2.2.4/css/dataTables.tableTools.css" rel="stylesheet"/>
+<script src="${pageContext.request.contextPath}/webjars/datatables-tools/2.2.4/js/dataTables.tableTools.js"></script>
+<link href="${pageContext.request.contextPath}/webjars/select2/4.0.0-rc.2/dist/css/select2.min.css" rel="stylesheet" />
+<script src="${pageContext.request.contextPath}/webjars/select2/4.0.0-rc.2/dist/js/select2.min.js"></script>						
+<script type="text/javascript" src="${pageContext.request.contextPath}/webjars/bootbox/4.4.0/bootbox.js" ></script>
+<script src="${pageContext.request.contextPath}/static/specifications/js/omnis.js"></script>
+
+
+
+<%-- TITLE --%>
+<div class="page-header">
+	<h1><spring:message code="label.registrationsDgesExport.searchRegistrationDGESStateBean" />
+		<small></small>
+	</h1>
+</div>
+<%-- NAVIGATION --%>
+	<c:if test="${not empty infoMessages}">
+				<div class="alert alert-info" role="alert">
+					
+					<c:forEach items="${infoMessages}" var="message"> 
+						<p> <span class="glyphicon glyphicon glyphicon-ok-sign" aria-hidden="true">&nbsp;</span>
+  							${message}
+  						</p>
+					</c:forEach>
+					
+				</div>	
+			</c:if>
+			<c:if test="${not empty warningMessages}">
+				<div class="alert alert-warning" role="alert">
+					
+					<c:forEach items="${warningMessages}" var="message"> 
+						<p> <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true">&nbsp;</span>
+  							${message}
+  						</p>
+					</c:forEach>
+					
+				</div>	
+			</c:if>
+			<c:if test="${not empty errorMessages}">
+				<div class="alert alert-danger" role="alert">
+					
+					<c:forEach items="${errorMessages}" var="message"> 
+						<p> <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true">&nbsp;</span>
+  							${message}
+  						</p>
+					</c:forEach>
+					
+				</div>	
+			</c:if>
+
+
+
+<form>
+		<select name="executionYear" style="width : 100px">
+			<c:forEach items="${executionYears}" var="executionYear">
+				<c:if test = "${executionYear == selectedExecutionYear}">
+					<option selected value="${executionYear.externalId}">${executionYear.name}</option>
+				</c:if>
+				<c:if test = "${executionYear != selectedExecutionYear}">
+					<option value="${executionYear.externalId}">${executionYear.name}</option>
+				</c:if>
+			</c:forEach>
+		</select>
+		<br />
+		<select name="phase" style="width : 100px">
+			<c:forEach items="${phases}" var="phase">
+				<c:if test = "${phase == selectedPhase}">
+					<option selected value="${phase}">${phase}</option>
+				</c:if>
+				<c:if test = "${phase != selectedPhase}">
+					<option value="${phase}">${phase}</option>
+				</c:if>
+			</c:forEach>
+		</select>
+		<br />
+	<input type="submit" value="Pesquisar" style="width : 100px">
+	<br />
+	<br />
+</form>
+<c:choose>
+	<c:when test="${not empty searchregistrationdgesstatebeanResultsDataSet}">
+		<table id="searchregistrationdgesstatebeanTable" class="table responsive table-bordered table-hover">
+			<thead>
+				<tr>
+					<th>Nº BI</th>
+					<th>Matrícula</th>
+					<th>S/N</th>
+				</tr>
+			</thead>
+			<tbody>
+				
+			</tbody>
+		</table>
+	</c:when>
+	<c:otherwise>
+				<div class="alert alert-warning" role="alert">
+					
+					<p> <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true">&nbsp;</span>			<spring:message code="label.noResultsFound" /></p>
+					
+				</div>	
+		
+	</c:otherwise>
+</c:choose>
+
+<script>
+	var searchregistrationdgesstatebeanDataSet = [
+			<c:forEach items="${searchregistrationdgesstatebeanResultsDataSet}" var="searchResult">
+				<%-- Field access / formatting  here CHANGE_ME --%>
+				{
+				"DT_RowId" : '<c:out value='${searchResult.name}'/>',
+"idnumber" : "<c:out value='${searchResult.idNumber}'/>",
+"name" : "<c:out value='${searchResult.name}'/>",
+"registrationstate" : "<c:out value='${searchResult.registrationState}'/>",
+			},
+            </c:forEach>
+    ];
+	
+	$(document).ready(function() {
+
+	
+
+
+		var table = $('#searchregistrationdgesstatebeanTable').DataTable({language : {
+			url : "${datatablesI18NUrl}",			
+		},
+		"columns": [
+			{ data: 'idnumber' },
+			{ data: 'name' },
+			{ data: 'registrationstate' },
+			
+		],
+		"data" : searchregistrationdgesstatebeanDataSet,
+"dom": '<"col-sm-6"l><"col-sm-3"f><"col-sm-3"T>rtip', //FilterBox = YES && ExportOptions = YES
+        "tableTools": {
+            "sSwfPath": "${pageContext.request.contextPath}/webjars/datatables-tools/2.2.4/swf/copy_csv_xls_pdf.swf"        	
+        }
+		});
+		table.columns.adjust().draw();
+		
+		  $('#searchregistrationdgesstatebeanTable tbody').on( 'click', 'tr', function () {
+		        $(this).toggleClass('selected');
+		    } );
+		  
+	}); 
+</script>
+
