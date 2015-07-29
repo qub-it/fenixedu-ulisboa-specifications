5d772cd86879daa314cab32ba55bd4f6c8e0638e
JoaoRoxo <joao.roxo@qub-it.com>
2015-07-29 11:01:12 +0100

    Added a few missing navigation elements to the courses enrollment, classes enrollment, and tuition information flows

--- src/main/java/org/fenixedu/ulisboa/specifications/ui/firstTimeCandidacy/ScheduleClassesController.java
+++ src/main/java/org/fenixedu/ulisboa/specifications/ui/firstTimeCandidacy/ScheduleClassesController.java
@@ -2,7 +2,6 @@ package org.fenixedu.ulisboa.specifications.ui.firstTimeCandidacy;
 
 import org.fenixedu.bennu.spring.portal.BennuSpringController;
 import org.fenixedu.ulisboa.specifications.ui.FenixeduUlisboaSpecificationsBaseController;
-import org.fenixedu.ulisboa.specifications.ui.FenixeduUlisboaSpecificationsController;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -11,10 +10,14 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping("/fenixedu-ulisboa-specifications/firsttimecandidacy/scheduleclasses")
 public class ScheduleClassesController extends FenixeduUlisboaSpecificationsBaseController {
 
-//				
     @RequestMapping
     public String scheduleclasses(Model model) {
         return "fenixedu-ulisboa-specifications/firsttimecandidacy/scheduleclasses";
+    }
+
+    @RequestMapping(value = "/openshiftenrollments")
+    public String scheduleclassesToOpenShiftEnrollments(Model model, RedirectAttributes redirectAttributes) {
+        return redirect("CHANGEME", model, redirectAttributes);
     }
 
     @RequestMapping(value = "/continue")
