63112d829768480d5892e84085d748b782452792
nuno.pinheiro <nuno.pinheiro@qub-it.com>
2015-08-08 01:15:43 +0100

    New feature: Add link for class enrolment in first time process

--- src/main/java/org/fenixedu/ulisboa/specifications/ui/firstTimeCandidacy/ScheduleClassesController.java
+++ src/main/java/org/fenixedu/ulisboa/specifications/ui/firstTimeCandidacy/ScheduleClassesController.java
@@ -1,10 +1,19 @@
 package org.fenixedu.ulisboa.specifications.ui.firstTimeCandidacy;
 
+import java.util.Optional;
+import java.util.function.Predicate;
+
+import org.fenixedu.academic.domain.ExecutionSemester;
+import org.fenixedu.academic.domain.student.Registration;
+import org.fenixedu.academic.predicate.AccessControl;
 import org.fenixedu.bennu.spring.portal.BennuSpringController;
+import org.fenixedu.ulisboa.specifications.domain.student.access.importation.DgesStudentImportationProcess;
 import org.fenixedu.ulisboa.specifications.ui.FenixeduUlisboaSpecificationsBaseController;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import pt.ist.fenixWebFramework.servlets.filters.contentRewrite.GenericChecksumRewriter;
 
 @BennuSpringController(value = InstructionsController.class)
 @RequestMapping("/fenixedu-ulisboa-specifications/firsttimecandidacy/scheduleclasses")
@@ -17,7 +26,27 @@ public class ScheduleClassesController extends FenixeduUlisboaSpecificationsBase
 
     @RequestMapping(value = "/openshiftenrollments")
     public String scheduleclassesToOpenShiftEnrollments(Model model, RedirectAttributes redirectAttributes) {
-        return redirect("CHANGEME", model, redirectAttributes);
+//        
+        ExecutionSemester executionSemester = ExecutionSemester.readActualExecutionSemester();
+
+        Predicate<? super Registration> hasDgesImportationProcessForCurrentYear =
+                DgesStudentImportationProcess.registrationHasDgesImportationProcessForCurrentYear();
+        Optional<Registration> findAny =
+                AccessControl.getPerson().getStudent().getRegistrationsSet().stream()
+                        .filter(hasDgesImportationProcessForCurrentYear).findAny();
+        if (findAny.isPresent()) {
+            Registration registration = findAny.get();
+            String link = "/student/studentShiftEnrollmentManager.do?method=start&executionSemesterID=%s&registrationOID=%s";
+            String format = String.format(link, executionSemester.getExternalId(), registration.getExternalId());
+
+            //request
+            String injectChecksumInUrl =
+                    GenericChecksumRewriter.injectChecksumInUrl(request.getContextPath(), format, request.getSession());
+            return redirect(injectChecksumInUrl, model, redirectAttributes);
+        } else {
+            //This should never happen, but strange things happen
+            throw new RuntimeException("Functionality only provided for candidates with current dges process");
+        }
     }
 
     @RequestMapping(value = "/continue")
