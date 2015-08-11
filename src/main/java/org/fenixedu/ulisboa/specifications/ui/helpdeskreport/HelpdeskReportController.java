09ab25171cec2e331d6927aae6daba2b1358cdc6
Diogo Simoes <diogo.simoes@qub-it.com>
2015-08-11 19:59:29 +0100

    HelpdeskReportController: Service implementation to handle helpdesk reports.
    Conflicts:
    	pom.xml

---
+++ src/main/java/org/fenixedu/ulisboa/specifications/ui/helpdeskreport/HelpdeskReportController.java
@@ -0,0 +1,194 @@
+package org.fenixedu.ulisboa.specifications.ui.helpdeskreport;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Base64;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+import javax.activation.DataHandler;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
+
+import org.apache.commons.validator.EmailValidator;
+import org.fenixedu.academic.FenixEduAcademicConfiguration;
+import org.fenixedu.academic.domain.Person;
+import org.fenixedu.academic.domain.person.RoleType;
+import org.fenixedu.academic.predicate.AccessControl;
+import org.fenixedu.academic.util.Bundle;
+import org.fenixedu.bennu.core.i18n.BundleUtil;
+import org.fenixedu.bennu.core.util.CoreConfiguration;
+import org.fenixedu.bennu.portal.domain.MenuFunctionality;
+import org.fenixedu.ulisboa.specifications.ui.FenixeduUlisboaSpecificationsBaseController;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.google.common.base.Objects;
+import com.google.common.base.Strings;
+
+import edu.emory.mathcs.backport.java.util.Arrays;
+
+@Controller
+@RequestMapping(HelpdeskReportController.CONTROLLER_URL)
+public class HelpdeskReportController extends FenixeduUlisboaSpecificationsBaseController {
+
+    public static final String CONTROLLER_URL = "/fenixedu-ulisboa-specifications/helpdeskreport";
+
+    private static final Logger logger = LoggerFactory.getLogger(HelpdeskReportController.class);
+
+    private static final String SEPARATOR =
+            "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n";
+
+    private static final String hostname = getHostName();
+
+    private static String getHostName() {
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            return "<Host name unknown>";
+        }
+    }
+
+    @RequestMapping(value = "/submitReport", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    public void submitReport(@RequestBody final HelpdeskReportForm jsonReportForm) {
+        String email = jsonReportForm.getEmail();
+        String mailSubject = generateEmailSubject(jsonReportForm);
+        String mailBody = generateEmailBody(jsonReportForm);
+
+        if (CoreConfiguration.getConfiguration().developmentMode()) {
+            logger.warn("Submitted error form from {}: '{}'\n{}", email, mailSubject, mailBody);
+        } else {
+            sendEmail(EmailValidator.getInstance().isValid(email) ? email : CoreConfiguration.getConfiguration()
+                    .defaultSupportEmailAddress(), mailSubject, mailBody, jsonReportForm);
+        }
+    }
+
+    private String generateEmailSubject(HelpdeskReportForm bean) {
+        StringBuilder builder = new StringBuilder();
+        MenuFunctionality functionality = bean.getMenuFunctionality();
+        builder.append("[Fenix] [");
+        builder.append(functionality != null ? functionality.getPathFromRoot().get(0).getTitle().getContent() : "").append("] ");
+        builder.append('[').append(bean.getType()).append("] ");
+        builder.append(bean.getSubject());
+        return builder.toString();
+    }
+
+    private String generateEmailBody(HelpdeskReportForm bean) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(SEPARATOR);
+        appendHeader(builder, bean);
+        appendBody(builder, bean);
+        return builder.toString();
+    }
+
+    private void appendBody(StringBuilder builder, HelpdeskReportForm bean) {
+        builder.append(SEPARATOR).append("\n");
+        builder.append(bean.getDescription()).append("\n\n\n\n");
+        if (!Strings.isNullOrEmpty(bean.getExceptionInfo())) {
+            builder.append(SEPARATOR).append(bean.getExceptionInfo());
+        }
+    }
+
+    private void appendHeader(StringBuilder builder, HelpdeskReportForm bean) {
+        generateLabel(builder, "Roles").append('[');
+        Person person = AccessControl.getPerson();
+        if (person != null) {
+            List<RoleType> roles = Arrays.asList(RoleType.values());
+            builder.append(roles.stream().filter(rt -> rt.isMember(person.getUser())).map(rt -> rt.getName())
+                    .reduce("", (a, b) -> a + " " + b).trim());
+        }
+        builder.append("]\n");
+
+        generateLabel(builder, "Name");
+        if (person != null) {
+            builder.append("[").append(person.getName()).append("]\n");
+            generateLabel(builder, "Username");
+            builder.append("[").append(person.getUsername()).append("]\n");
+        } else {
+            builder.append(BundleUtil.getString(Bundle.APPLICATION, "support.mail.session.error")).append('\n');
+        }
+
+        generateLabel(builder, "Email").append("[").append(bean.getEmail()).append("]\n");
+
+        // Portal
+        MenuFunctionality functionality = bean.getMenuFunctionality();
+        generateLabel(builder, "Portal").append("[");
+        if (functionality != null) {
+            builder.append(functionality.getPathFromRoot().stream().map(item -> item.getTitle().getContent())
+                    .collect(Collectors.joining(" > ")));
+        }
+        builder.append("]\n");
+
+        generateLabel(builder, "Referer").append('[').append(bean.getReferer()).append("]\n");
+        generateLabel(builder, "Browser/OS").append("[").append(bean.getUserAgent()).append("]\n");
+
+        generateLabel(builder, "Type").append('[').append(bean.getType().toUpperCase()).append("]\n");
+
+        // Extra Info
+        generateLabel(builder, "When").append('[').append(DateTime.now()).append("]\n");
+        generateLabel(builder, "Host").append('[').append(hostname).append("]\n");
+    }
+
+    private StringBuilder generateLabel(StringBuilder builder, String label) {
+        builder.append(label);
+        for (int i = label.length(); i <= 15; i++) {
+            builder.append('_');
+        }
+        return builder;
+    }
+
+    private void sendEmail(String from, String subject, String body, HelpdeskReportForm bean) {
+        Properties props = new Properties();
+        props.put("mail.smtp.host",
+                Objects.firstNonNull(FenixEduAcademicConfiguration.getConfiguration().getMailSmtpHost(), "localhost"));
+        Session session = Session.getDefaultInstance(props, null);
+        MimeMessage message = new MimeMessage(session);
+        try {
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(CoreConfiguration.getConfiguration()
+                    .defaultSupportEmailAddress()));
+            message.setSubject(subject);
+            message.setText(body);
+
+            Multipart multipart = new MimeMultipart();
+            {
+                BodyPart messageBodyPart = new MimeBodyPart();
+                messageBodyPart.setText(body);
+                multipart.addBodyPart(messageBodyPart);
+            }
+
+            if (!Strings.isNullOrEmpty(bean.getAttachment())) {
+                BodyPart messageBodyPart = new MimeBodyPart();
+                messageBodyPart.setDataHandler(new DataHandler(new ByteArrayDataSource(Base64.getDecoder().decode(
+                        bean.getAttachment()), bean.getMimeType())));
+                messageBodyPart.setFileName(bean.getFileName());
+                multipart.addBodyPart(messageBodyPart);
+            }
+
+            message.setContent(multipart);
+
+            Transport.send(message);
+        } catch (Exception e) {
+            logger.error("Could not send support email! Original message was: " + body, e);
+        }
+    }
+
+}
---
+++ src/main/java/org/fenixedu/ulisboa/specifications/ui/helpdeskreport/HelpdeskReportForm.java
@@ -0,0 +1,124 @@
+package org.fenixedu.ulisboa.specifications.ui.helpdeskreport;
+
+import java.io.Serializable;
+
+import org.fenixedu.bennu.core.security.Authenticate;
+import org.fenixedu.bennu.portal.domain.MenuFunctionality;
+
+import pt.ist.fenixframework.FenixFramework;
+
+import com.google.common.base.Strings;
+
+public class HelpdeskReportForm implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private String subject;
+    private String description;
+    private String type = "exception";
+    private String exceptionInfo;
+    private String functionality;
+    private String referer;
+    private String userAgent;
+    private String attachment;
+    private String fileName;
+    private String mimeType;
+    private String email;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getExceptionInfo() {
+        return exceptionInfo;
+    }
+
+    public void setExceptionInfo(String exceptionInfo) {
+        this.exceptionInfo = exceptionInfo;
+    }
+
+    public String getFunctionality() {
+        return functionality;
+    }
+
+    public void setFunctionality(String functionality) {
+        this.functionality = functionality;
+    }
+
+    public String getReferer() {
+        return referer;
+    }
+
+    public void setReferer(String referer) {
+        this.referer = referer;
+    }
+
+    public String getUserAgent() {
+        return userAgent;
+    }
+
+    public void setUserAgent(String userAgent) {
+        this.userAgent = userAgent;
+    }
+
+    public String getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(String attachment) {
+        this.attachment = attachment;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public String getEmail() {
+        if (!Strings.isNullOrEmpty(email)) {
+            return email;
+        } else if (Authenticate.isLogged()) {
+            return Authenticate.getUser().getEmail();
+        } else {
+            return "noreply@qub-it.com";
+        }
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public MenuFunctionality getMenuFunctionality() {
+        return Strings.isNullOrEmpty(functionality) ? null : FenixFramework.getDomainObject(functionality);
+    }
+}
