1b42b52650fc05d87266aac1cf8cfba9ad588df9
Diogo Simoes <diogo.simoes@qub-it.com>
2015-08-17 19:20:18 +0100

    Created a Configurations object for Helpdesk. Reconfigured mailinglist for HelpdeskReport.

--- src/main/java/org/fenixedu/ulisboa/specifications/task/ConfigureSupportEmails.java
+++ src/main/java/org/fenixedu/ulisboa/specifications/task/ConfigureSupportEmails.java
@@ -2,11 +2,14 @@ package org.fenixedu.ulisboa.specifications.task;
 
 import org.fenixedu.academic.domain.Installation;
 import org.fenixedu.bennu.scheduler.custom.CustomTask;
+import org.fenixedu.ulisboa.specifications.domain.helpdesk.HelpdeskConfigurations;
 
-public class ConfigureAdminOfficeSupportEmail extends CustomTask {
+public class ConfigureSupportEmails extends CustomTask {
 
     @Override
     public void runTask() throws Exception {
+        HelpdeskConfigurations.getInstance().addBCC("support@qub-it.com");
+        
         /*FMV*/Installation.getInstance().setAcademicEmailAddress("secretaria@fmv.ulisboa.pt");
 //*FF */ Installation.getInstance().setAcademicEmailAddress("academicos@ff.ulisboa.pt");
 //*FMD*/ Installation.getInstance().setAcademicEmailAddress("secretaria@fmd.ulisboa.pt");
