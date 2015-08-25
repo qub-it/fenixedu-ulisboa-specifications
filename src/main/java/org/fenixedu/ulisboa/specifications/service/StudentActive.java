f418f93f1c36dde6de10ca0bc213d46e4f53f7bd
Paulo Abrantes <paulo.abrantes@qub-it.com>
2015-08-25 12:10:00 +0100

    Added centralized logic to understand if the student is active or not. This is both used by the ldap connector and the sas webservices

---
+++ src/main/java/org/fenixedu/ulisboa/specifications/service/StudentActive.java
@@ -0,0 +1,108 @@
+/**
+ * This file was created by Quorum Born IT <http://www.qub-it.com/> and its 
+ * copyright terms are bind to the legal agreement regulating the FenixEdu@ULisboa 
+ * software development project between Quorum Born IT and Serviços Partilhados da
+ * Universidade de Lisboa:
+ *  - Copyright © 2015 Quorum Born IT (until any Go-Live phase)
+ *  - Copyright © 2015 Universidade de Lisboa (after any Go-Live phase)
+ *
+ * Contributors: paulo.abrantes@qub-it.com
+ *
+ * 
+ * This file is part of FenixEdu fenixedu-ulisboa-specifications.
+ *
+ * FenixEdu fenixedu-ulisboa-specifications is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FenixEdu fenixedu-ulisboa-specifications is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with FenixEdu fenixedu-ulisboa-specifications.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.fenixedu.ulisboa.specifications.service;
+
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import org.fenixedu.academic.domain.ExecutionYear;
+import org.fenixedu.academic.domain.candidacy.StudentCandidacy;
+import org.fenixedu.academic.domain.student.Registration;
+import org.fenixedu.academic.domain.student.Student;
+import org.joda.time.YearMonthDay;
+
+public class StudentActive {
+
+    public static boolean isActiveStudent(Student student) {
+        boolean hasActiveRegistrationsWithEnrolments = false;
+        boolean activeRegistrationCreatedInTheLastMonth = false;
+        ExecutionYear currentExecutionYear = ExecutionYear.readCurrentExecutionYear();
+        ExecutionYear previousExecutionYear = currentExecutionYear.getPreviousExecutionYear();
+        if (student != null) {
+            Predicate<? super Registration> registrationHasEnrolmentsInLast2Years =
+                    registration -> !registration.getEnrolments(currentExecutionYear).isEmpty()
+                            || !registration.getEnrolments(previousExecutionYear).isEmpty();
+            hasActiveRegistrationsWithEnrolments =
+                    !student.getActiveRegistrations().stream().filter(registrationHasEnrolmentsInLast2Years)
+                            .collect(Collectors.toList()).isEmpty();
+
+            if (!hasActiveRegistrationsWithEnrolments) {
+                // There are special kinds of students, for example in protocols, where they are created in Fenix,
+                // without a candidacy and where they have no enrolments. Although we still want them to be considered 
+                // active students so the attribute ULStudentActive is sent to LDAP with TRUE so the campus account is 
+                // created.
+                //
+                // To take into account these special cases we'll check active registrations for the student and
+                // see if any of them were created in the last month, if so we'll consider it's an active student.
+                //
+                // This solution was discussed with João Rafael and Daniela Mendes from ULisboa.
+                //
+                // 21 August 2015 - Paulo Abrantes
+                YearMonthDay today = new YearMonthDay();
+                YearMonthDay lastMonth = today.minusMonths(1);
+                YearMonthDay tomorrow = today.plusDays(1);
+                activeRegistrationCreatedInTheLastMonth =
+                        !student.getActiveRegistrations()
+                                .stream()
+                                .filter(registration -> registration.getStartDate() != null
+                                        && registration.getStartDate().isAfter(lastMonth)
+                                        && registration.getStartDate().isBefore(tomorrow)).collect(Collectors.toList()).isEmpty();
+            }
+        }
+        //
+        // Detect if it's 1st year, 1st time
+        //
+        boolean isFirstYearFirstTime =
+                student.getPerson()
+                        .getCandidaciesSet()
+                        .stream()
+                        .filter(candidacy -> candidacy instanceof StudentCandidacy)
+                        .map(StudentCandidacy.class::cast)
+                        .anyMatch(
+                                studentCandidacy -> studentCandidacy.isActive() && studentCandidacy.getEntryPhase() != null
+                                        && studentCandidacy.getExecutionYear() == currentExecutionYear);
+
+        // Removing detecion of other kind of candidate (still leaving the code just in case) 
+        // Ana Rute asked us to not send other candidates has active students, only 1stYearFirstTime.
+        // This will be sent with the student flag active (and then synchronized with IDM) when they 
+        // finalize their candidacy situation and become an actual student
+        //
+        // 27 July 2015 - Paulo Abrantes
+        boolean isOtherKindOfCandidate =
+                student.getPerson()
+                        .getCandidaciesSet()
+                        .stream()
+                        .filter(candidacy -> candidacy instanceof StudentCandidacy)
+                        .map(StudentCandidacy.class::cast)
+                        .anyMatch(
+                                candidacy -> candidacy.getRegistration() != null && candidacy.getRegistration().isActive()
+                                        && candidacy.getExecutionYear() == currentExecutionYear);
+
+        return hasActiveRegistrationsWithEnrolments || isFirstYearFirstTime || activeRegistrationCreatedInTheLastMonth;
+    }
+
+}
