c5ba9aed7983e3b36aea2bcdfcb22839c106089c
Shezad Anavarali <shezad.anavarali@qub-it.com>
2015-08-31 12:38:34 +0100

    added scripts to list and manage school class registrations

---
+++ src/main/java/org/fenixedu/ulisboa/specifications/task/ListRegistrationsBySchoolClass.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package org.fenixedu.ulisboa.specifications.task;
+
+import org.fenixedu.academic.domain.ExecutionSemester;
+import org.fenixedu.academic.domain.ExecutionYear;
+import org.fenixedu.academic.domain.SchoolClass;
+import org.fenixedu.academic.domain.student.Registration;
+import org.fenixedu.academic.domain.student.Student;
+import org.fenixedu.bennu.scheduler.CronTask;
+import org.fenixedu.bennu.scheduler.annotation.Task;
+
+/**
+ * @author shezad - Aug 31, 2015
+ *
+ */
+@Task(englishTitle = "List Students enroled in each SchoolClass", readOnly = true)
+public class ListRegistrationsBySchoolClass extends CronTask {
+
+    @Override
+    public void runTask() throws Exception {
+
+        for (final ExecutionSemester executionSemester : ExecutionYear.readCurrentExecutionYear().getExecutionPeriodsSet()) {
+            taskLog("\n== %s ==\n\n", executionSemester.getQualifiedName());
+
+            for (final SchoolClass schoolClass : executionSemester.getSchoolClassesSet()) {
+                taskLog("[%s - %dA] %s - %d STUDENTS (OID: %s)\n", schoolClass.getEditablePartOfName(),
+                        schoolClass.getAnoCurricular(), schoolClass.getExecutionDegree().getPresentationName(), schoolClass
+                                .getRegistrationsSet().size(), schoolClass.getExternalId());
+
+                for (Registration registration : schoolClass.getRegistrationsSet()) {
+                    final Student student = registration.getStudent();
+                    taskLog("_ _ _ [%d] %s (OID: %s)\n", student.getNumber(), student.getName(), registration.getExternalId());
+                }
+            }
+
+        }
+
+    }
+
+}
---
+++ src/main/java/org/fenixedu/ulisboa/specifications/task/ManageRegistrationSchoolClasses.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package org.fenixedu.ulisboa.specifications.task;
+
+import java.util.Optional;
+
+import org.fenixedu.academic.domain.ExecutionSemester;
+import org.fenixedu.academic.domain.SchoolClass;
+import org.fenixedu.academic.domain.student.Registration;
+import org.fenixedu.academic.domain.student.Student;
+import org.fenixedu.bennu.scheduler.custom.CustomTask;
+
+import pt.ist.fenixframework.FenixFramework;
+
+/**
+ * @author shezad - Aug 31, 2015
+ *
+ */
+public class ManageRegistrationSchoolClasses extends CustomTask {
+
+    @Override
+    public void runTask() throws Exception {
+
+        final Registration registration = FenixFramework.getDomainObject("REGISTRATION OID");
+//        final SchoolClass schoolClass = FenixFramework.getDomainObject("SCHOOL_CLASS OID");
+        final SchoolClass schoolClass = null;
+        final ExecutionSemester executionSemester =
+                schoolClass != null ? schoolClass.getExecutionPeriod() : FenixFramework.getDomainObject("SEMESTER OID");
+
+        if (registration == null) {
+            taskLog("Error: unable to detect registration");
+            return;
+        }
+
+        final Student student = registration.getStudent();
+        taskLog("[%d] %s\n", student.getNumber(), student.getName());
+
+        if (executionSemester != null) {
+            final Optional<SchoolClass> oldSchoolClassOptional = registration.getSchoolClassBy(executionSemester);
+            if (oldSchoolClassOptional.isPresent()) {
+                final SchoolClass oldSchoolClass = oldSchoolClassOptional.get();
+                taskLog("Old SchoolClass: [%s - %dA] %s\n", oldSchoolClass.getEditablePartOfName(),
+                        oldSchoolClass.getAnoCurricular(), oldSchoolClass.getExecutionDegree().getPresentationName());
+            }
+            registration.replaceSchoolClass(schoolClass, executionSemester);
+            if (schoolClass != null) {
+                taskLog("New SchoolClass: [%s - %dA] %s\n", schoolClass.getEditablePartOfName(), schoolClass.getAnoCurricular(),
+                        schoolClass.getExecutionDegree().getPresentationName());
+            } else {
+                taskLog("New SchoolClass: EMPTY\n");
+            }
+
+        } else {
+            taskLog("Error: unable to detect execution semester");
+        }
+
+    }
+
+}
