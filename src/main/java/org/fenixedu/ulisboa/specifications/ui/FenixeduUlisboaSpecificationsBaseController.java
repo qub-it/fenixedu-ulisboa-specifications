e4c89e57c018c9f1f9230bab779b66de1e7835ec
Nadir Amin <nadir.amin@qub-it.com>
2015-11-20 16:28:57 +0000

    Added support to manage AnyCurricularcourseExceptionsConfiguration

--- src/main/java/org/fenixedu/ulisboa/specifications/ui/FenixeduUlisboaSpecificationsBaseController.java
+++ src/main/java/org/fenixedu/ulisboa/specifications/ui/FenixeduUlisboaSpecificationsBaseController.java
@@ -1,51 +1,24 @@
 package org.fenixedu.ulisboa.specifications.ui;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.fenixedu.bennu.spring.FenixEDUBaseController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-public class FenixeduUlisboaSpecificationsBaseController {
-    private static final String ERROR_MESSAGES = "errorMessages";
-    private static final String WARNING_MESSAGES = "warningMessages";
-    private static final String INFO_MESSAGES = "infoMessages";
+public class FenixeduUlisboaSpecificationsBaseController extends FenixEDUBaseController {
 
     //The HTTP Request that can be used internally in the controller
     protected @Autowired HttpServletRequest request;
 
-    //The entity in the Model
-
-    // The list of INFO messages that can be showed on View
-    protected void addInfoMessage(String message, Model model) {
-        ((List<String>) model.asMap().get(INFO_MESSAGES)).add(message);
-    }
-
-    // The list of WARNING messages that can be showed on View
-    protected void addWarningMessage(String message, Model model) {
-        ((List<String>) model.asMap().get(WARNING_MESSAGES)).add(message);
-    }
-
-    // The list of ERROR messages that can be showed on View
-    protected void addErrorMessage(String message, Model model) {
-        ((List<String>) model.asMap().get(ERROR_MESSAGES)).add(message);
-    }
-
-    protected void clearMessages(Model model) {
-        model.addAttribute(INFO_MESSAGES, new ArrayList<String>());
-        model.addAttribute(WARNING_MESSAGES, new ArrayList<String>());
-        model.addAttribute(ERROR_MESSAGES, new ArrayList<String>());
-    }
-
     @ModelAttribute
     protected void addModelProperties(Model model) {
-        model.addAttribute(INFO_MESSAGES, new ArrayList<String>());
-        model.addAttribute(WARNING_MESSAGES, new ArrayList<String>());
-        model.addAttribute(ERROR_MESSAGES, new ArrayList<String>());
+        super.addModelProperties(model, request);
 
         String infoMessages = request.getParameter(INFO_MESSAGES);
         if (infoMessages != null) {
@@ -59,23 +32,21 @@ public class FenixeduUlisboaSpecificationsBaseController {
         if (errorMessages != null) {
             addErrorMessage(errorMessages, model);
         }
-        //Add here more attributes to the Model
-        //model.addAttribute(<attr1Key>, <attr1Value>);
-        //....
+
     }
 
-    protected String redirect(String destinationAction, Model model, RedirectAttributes redirectAttributes) {
-        if (model.containsAttribute(INFO_MESSAGES)) {
-            redirectAttributes.addFlashAttribute(INFO_MESSAGES, model.asMap().get(INFO_MESSAGES));
-        }
-        if (model.containsAttribute(WARNING_MESSAGES)) {
-            redirectAttributes.addFlashAttribute(WARNING_MESSAGES, model.asMap().get(WARNING_MESSAGES));
-        }
-        if (model.containsAttribute(ERROR_MESSAGES)) {
-            redirectAttributes.addFlashAttribute(ERROR_MESSAGES, model.asMap().get(ERROR_MESSAGES));
-        }
+    protected void writeFile(final HttpServletResponse response, final String filename, final String contentType,
+            final byte[] content) throws IOException {
 
-        return "redirect:" + destinationAction;
+        response.setContentLength(content.length);
+        response.setContentType(contentType);
+        response.addHeader("Content-Disposition", "attachment; filename=\"" + filename + "\"");
+
+        try (ServletOutputStream outputStream = response.getOutputStream()) {
+            outputStream.write(content);
+            outputStream.flush();
+            response.flushBuffer();
+        }
     }
 
 }
---
+++ src/main/java/org/fenixedu/ulisboa/specifications/util/ULisboaSpecificationsUtil.java
@@ -0,0 +1,51 @@
+/**
+ * This file was created by Quorum Born IT <http://www.qub-it.com/> and its 
+ * copyright terms are bind to the legal agreement regulating the FenixEdu@ULisboa 
+ * software development project between Quorum Born IT and Serviços Partilhados da
+ * Universidade de Lisboa:
+ *  - Copyright © 2015 Quorum Born IT (until any Go-Live phase)
+ *  - Copyright © 2015 Universidade de Lisboa (after any Go-Live phase)
+ *
+ * Contributors: ricardo.pedro@qub-it.com, anil.mamede@qub-it.com
+ *
+ * 
+ * This file is part of FenixEdu Treasury.
+ *
+ * FenixEdu Treasury is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FenixEdu Treasury is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with FenixEdu Treasury.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.fenixedu.ulisboa.specifications.util;
+
+import org.fenixedu.bennu.FenixeduUlisboaSpecificationsSpringConfiguration;
+import org.fenixedu.bennu.core.i18n.BundleUtil;
+import org.fenixedu.commons.i18n.LocalizedString;
+
+public class ULisboaSpecificationsUtil {
+
+    public static final String BUNDLE = FenixeduUlisboaSpecificationsSpringConfiguration.BUNDLE.replace('/', '.');
+
+    // @formatter: off
+    /**********
+     * BUNDLE *
+     **********/
+    // @formatter: on
+
+    public static String bundle(final String key, final String... args) {
+        return BundleUtil.getString(ULisboaSpecificationsUtil.BUNDLE, key, args);
+    }
+
+    public static LocalizedString bundleI18N(final String key, final String... args) {
+        return BundleUtil.getLocalizedString(ULisboaSpecificationsUtil.BUNDLE, key, args);
+    }
+
+}
