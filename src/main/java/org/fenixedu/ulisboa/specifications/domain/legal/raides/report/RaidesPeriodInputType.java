3d50bb90d39d4512c68f49d072fff593d55428ac
JNPA <johndoe.amaral@gmail.com>
2016-01-21 13:02:41 -0100

    Add read screen for RAIDES requests

--- src/main/java/org/fenixedu/ulisboa/specifications/domain/legal/raides/report/RaidesPeriodInputType.java
+++ src/main/java/org/fenixedu/ulisboa/specifications/domain/legal/raides/report/RaidesPeriodInputType.java
@@ -1,19 +1,29 @@
 package org.fenixedu.ulisboa.specifications.domain.legal.raides.report;
 
+import org.fenixedu.bennu.core.i18n.BundleUtil;
+import org.fenixedu.ulisboa.specifications.util.ULisboaConstants;
+
 public enum RaidesPeriodInputType {
-    ENROLLED,
-    GRADUATED,
-    INTERNATIONAL_MOBILITY;
+    ENROLLED, GRADUATED, INTERNATIONAL_MOBILITY;
 
     public boolean isForEnrolled() {
         return this == ENROLLED;
     }
-    
+
     public boolean isForGraduated() {
         return this == GRADUATED;
     }
-    
+
     public boolean isForInternationalMobility() {
         return this == INTERNATIONAL_MOBILITY;
     }
+
+    public String getQualifiedName() {
+        return RaidesPeriodInputType.class.getSimpleName() + "." + name();
+    }
+
+    public String getLocalizedName() {
+        return BundleUtil.getString(ULisboaConstants.BUNDLE, getQualifiedName());
+    }
+
 }
