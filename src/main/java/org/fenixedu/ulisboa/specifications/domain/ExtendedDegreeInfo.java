cbc72b8d2e64c3b0fe0cd514d4132218d066d48f
Diogo Simoes <diogo.simoes@qub-it.com>
2016-03-21 19:04:16 +0000

    DegreeInfo extension mechanisms.

--- src/main/java/org/fenixedu/ulisboa/specifications/domain/ExtendedDegreeInfo.java
+++ src/main/java/org/fenixedu/ulisboa/specifications/domain/ExtendedDegreeInfo.java
@@ -1,15 +1,77 @@
 package org.fenixedu.ulisboa.specifications.domain;
 
+import org.fenixedu.academic.domain.Degree;
 import org.fenixedu.academic.domain.DegreeInfo;
+import org.fenixedu.academic.domain.ExecutionYear;
+import org.fenixedu.bennu.core.domain.Bennu;
+import org.fenixedu.bennu.signals.DomainObjectEvent;
+import org.fenixedu.bennu.signals.Signal;
+import pt.ist.fenixframework.Atomic;
+import pt.ist.fenixframework.FenixFramework;
 
 public class ExtendedDegreeInfo extends ExtendedDegreeInfo_Base {
 
+    public static void setupDeleteListener() {
+        FenixFramework.getDomainModel().registerDeletionListener(DegreeInfo.class, degreeInfo -> {
+            ExtendedDegreeInfo edi = degreeInfo.getExtendedDegreeInfo();
+            degreeInfo.setExtendedDegreeInfo(null);
+            edi.delete();
+        });
+    }
+
+    public static void setupCreationListener() {
+        Signal.register(DegreeInfo.DEGREE_INFO_CREATION_EVENT, (DomainObjectEvent<DegreeInfo> event) -> {
+            DegreeInfo degreeInfo = event.getInstance();
+            new ExtendedDegreeInfo(degreeInfo); // should read last defined edi
+            });
+    }
+
     public ExtendedDegreeInfo() {
         super();
+        setBennu(Bennu.getInstance());
     }
 
     public ExtendedDegreeInfo(DegreeInfo degreeInfo) {
+        this();
         setDegreeInfo(degreeInfo);
+    }
+
+    public void delete() {
+        setDegreeInfo(null);
+        setBennu(null);
+        deleteDomainObject();
+    }
+
+    /**
+     * @return The ExtendedDegreeInfo instance associated with the MOST recent DegreeInfo.
+     */
+    @Atomic
+    public static ExtendedDegreeInfo readMostRecent(ExecutionYear executionYear, Degree degree) {
+        DegreeInfo di = degree.getMostRecentDegreeInfo(executionYear);
+        if (di.getExtendedDegreeInfo() == null) {
+            return new ExtendedDegreeInfo(di); // should read last defined edi
+        }
+        return di.getExtendedDegreeInfo();
+    }
+
+    /**
+     * @return The ExtendedDegreeInfo instance associated with a DegreeInfo instance specific to the specified ExecutionYear. If
+     *         no such DegreeInfo exists, a new one is created by cloning the most recent DegreeInfo. The same process is applied
+     *         to the ExtendedDegreeInfo.
+     */
+    @Atomic
+    public static ExtendedDegreeInfo getOrCreate(ExecutionYear executionYear, Degree degree) {
+        DegreeInfo di = executionYear.getDegreeInfo(degree);
+        if (di != null) {
+            if (di.getExtendedDegreeInfo() == null) {
+                return new ExtendedDegreeInfo(di); // should read last defined edi
+            }
+            return di.getExtendedDegreeInfo();
+        }
+        DegreeInfo mrdi = degree.getMostRecentDegreeInfo(executionYear);
+        di = mrdi != null ? new DegreeInfo(mrdi, executionYear) : new DegreeInfo(degree, executionYear);
+        return di.getExtendedDegreeInfo();
+
     }
 
 }
--- src/main/java/org/fenixedu/ulisboa/specifications/ui/degrees/extendeddegreeinfo/ExtendedDegreeInfoController.java
+++ src/main/java/org/fenixedu/ulisboa/specifications/ui/degrees/extendeddegreeinfo/ExtendedDegreeInfoController.java
@@ -1,11 +1,7 @@
 package org.fenixedu.ulisboa.specifications.ui.degrees.extendeddegreeinfo;
 
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-
 import org.fenixedu.bennu.spring.portal.SpringFunctionality;
-import org.fenixedu.commons.i18n.LocalizedString;
+import org.fenixedu.ulisboa.specifications.domain.ExtendedDegreeInfo;
 import org.fenixedu.ulisboa.specifications.ui.FenixeduUlisboaSpecificationsBaseController;
 import org.fenixedu.ulisboa.specifications.ui.FenixeduUlisboaSpecificationsController;
 import org.springframework.ui.Model;
@@ -20,15 +16,13 @@ public class ExtendedDegreeInfoController extends FenixeduUlisboaSpecificationsB
     public static final String CONTROLLER_URL = "/fenixedu-ulisboa-specifications/degrees/extendeddegreeinfo";
     public static final String VIEWS_URL = "fenixedu-ulisboa-specifications/degrees/extendeddegreeinfo";
 
-    public static final Map<String, DummyEDI> cache = new HashMap<String, DummyEDI>();
-
     @RequestMapping
     public String home() {
         return "forward:" + SEARCH_URL;
     }
 
     private void setExtendedDegreeInfoBean(ExtendedDegreeInfoBean bean, Model model) {
-        map(bean);
+        load(bean);
         bean.setDegreeType(bean.getDegree().getDegreeTypeName());
         bean.setDegreeAcron(bean.getDegree().getSigla());
         model.addAttribute("extendedDegreeInfoBeanJson", getBeanJson(bean));
@@ -67,44 +61,26 @@ public class ExtendedDegreeInfoController extends FenixeduUlisboaSpecificationsB
         return VIEWS_URL + "/angular";
     }
 
-    private void map(ExtendedDegreeInfoBean bean) {
-        String key = bean.getExecutionYear().getExternalId() + "+" + bean.getDegree().getExternalId();
-        if (!cache.containsKey(key)) {
-            DummyEDI edi = new DummyEDI();
-            edi.scientificAreas =
-                    new LocalizedString(new Locale("pt", "PT"), bean.getExecutionYear().getBeginCivilYear() + "/"
-                            + bean.getExecutionYear().getEndCivilYear());
-            cache.put(key, edi);
-        }
-        DummyEDI edi = cache.get(key);
-        bean.setScientificAreas(edi.scientificAreas);
-        bean.setStudyRegime(edi.studyRegime);
-        bean.setStudyProgrammeRequirements(edi.studyProgrammeRequirements);
-        bean.setHigherEducationAccess(edi.higherEducationAccess);
-        bean.setProfessionalStatus(edi.professionalStatus);
-        bean.setSupplementExtraInformation(edi.supplementExtraInformation);
-        bean.setSupplementOtherSources(edi.supplementOtherSources);
+    private void load(ExtendedDegreeInfoBean bean) {
+        ExtendedDegreeInfo edi = ExtendedDegreeInfo.readMostRecent(bean.getExecutionYear(), bean.getDegree());
+        bean.setScientificAreas(edi.getScientificAreas());
+        bean.setStudyRegime(edi.getStudyRegime());
+        bean.setStudyProgrammeRequirements(edi.getStudyProgrammeRequirements());
+        bean.setHigherEducationAccess(edi.getHigherEducationAccess());
+        bean.setProfessionalStatus(edi.getProfessionalStatus());
+        bean.setSupplementExtraInformation(edi.getSupplementExtraInformation());
+        bean.setSupplementOtherSources(edi.getSupplementOtherSources());
     }
 
     private void store(ExtendedDegreeInfoBean bean) {
-        String key = bean.getExecutionYear().getExternalId() + "+" + bean.getDegree().getExternalId();
-        DummyEDI edi = cache.get(key);
-        edi.scientificAreas = bean.getScientificAreas();
-        edi.studyRegime = bean.getStudyRegime();
-        edi.studyProgrammeRequirements = bean.getStudyProgrammeRequirements();
-        edi.higherEducationAccess = bean.getHigherEducationAccess();
-        edi.professionalStatus = bean.getProfessionalStatus();
-        edi.supplementExtraInformation = bean.getSupplementExtraInformation();
-        edi.supplementOtherSources = bean.getSupplementOtherSources();
+        ExtendedDegreeInfo edi = ExtendedDegreeInfo.getOrCreate(bean.getExecutionYear(), bean.getDegree());
+        edi.setScientificAreas(bean.getScientificAreas());
+        edi.setStudyRegime(bean.getStudyRegime());
+        edi.setStudyProgrammeRequirements(bean.getStudyProgrammeRequirements());
+        edi.setHigherEducationAccess(bean.getHigherEducationAccess());
+        edi.setProfessionalStatus(bean.getProfessionalStatus());
+        edi.setSupplementExtraInformation(bean.getSupplementExtraInformation());
+        edi.setSupplementOtherSources(bean.getSupplementOtherSources());
     }
 
-    private static class DummyEDI {
-        LocalizedString scientificAreas;
-        LocalizedString studyRegime;
-        LocalizedString studyProgrammeRequirements;
-        LocalizedString higherEducationAccess;
-        LocalizedString professionalStatus;
-        LocalizedString supplementExtraInformation;
-        LocalizedString supplementOtherSources;
-    }
 }
