6d135d2c43ca4fde894bf7f72156294012dbd203
Diogo Simoes <diogo.simoes@qub-it.com>
2016-03-28 19:24:48 +0100

    Minor refactoring on GradingTableData WIP.

--- src/main/java/org/fenixedu/ulisboa/specifications/domain/ects/tasks/TestGradingTableData.java
+++ src/main/java/org/fenixedu/ulisboa/specifications/domain/ects/tasks/TestGradingTableData.java
@@ -1,6 +1,10 @@
 package org.fenixedu.ulisboa.specifications.domain.ects.tasks;
 
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.fenixedu.bennu.scheduler.custom.CustomTask;
 import org.fenixedu.ulisboa.specifications.domain.ects.GradingTableData;
@@ -11,7 +15,7 @@ public class TestGradingTableData extends CustomTask {
     @Override
     public void runTask() throws Exception {
         GradingTableData gradingTable = new GradingTableData();
-        List<GradeConversion> data = gradingTable.getTable();
+        Set<GradeConversion> data = gradingTable.getTable();
         data.add(new GradeConversion("10", "E"));
         data.add(new GradeConversion("11", "E"));
         data.add(new GradeConversion("14", "C"));
@@ -19,10 +23,14 @@ public class TestGradingTableData extends CustomTask {
         print(data);
 
         gradingTable.setTable(data);
+        reorder(gradingTable);
         String json = gradingTable.toJson();
         print(json);
 
         GradingTableData deserialTable = GradingTableData.fromJson(json);
+        print(deserialTable.getTable());
+        deserialTable.getTable().add(new GradeConversion("9", "F"));
+        deserialTable.getTable().add(new GradeConversion("20", "A"));
         print(deserialTable.getTable());
 
         String json2 = deserialTable.toJson();
@@ -31,11 +39,13 @@ public class TestGradingTableData extends CustomTask {
         taskLog("--Finished--");
     }
 
-    private void print(final List<GradeConversion> data) {
+    private void print(final Set<GradeConversion> data) {
         taskLog("[");
+        Iterator<GradeConversion> iter = data.iterator();
         for (int index = 0; index < data.size(); index++) {
+            GradeConversion gc = iter.next();
             taskLog("\t" + index + ": {");
-            taskLog("\t\t\"" + data.get(index).getMark() + "\" : \"" + data.get(index).getEctsGrade() + "\"");
+            taskLog("\t\t\"" + gc.getMark() + "\" : \"" + gc.getEctsGrade() + "\"");
             taskLog("\t}");
         }
         taskLog("]");
@@ -43,6 +53,17 @@ public class TestGradingTableData extends CustomTask {
 
     private void print(String json) {
         taskLog(json);
+    }
+
+    private void reorder(GradingTableData tableData) {
+        Set<GradeConversion> table = new TreeSet<GradeConversion>(new Comparator<GradeConversion>() {
+            @Override
+            public int compare(GradeConversion gc1, GradeConversion gc2) {
+                return gc2.getMark().compareTo(gc1.getMark());
+            }
+        });
+        table.addAll(tableData.getTable());
+        tableData.setTable(table);
     }
 
 }
