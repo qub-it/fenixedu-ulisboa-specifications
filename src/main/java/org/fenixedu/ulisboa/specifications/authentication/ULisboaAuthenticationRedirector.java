ddee76cc6ccc06c232f11627fee18f0b8d8bd162
Shezad Anavarali <shezad.anavarali@qub-it.com>
2016-04-19 09:43:47 +0100

    method getRedirectionPath(..) now returns null if no result found

--- src/main/java/org/fenixedu/ulisboa/specifications/authentication/ULisboaAuthenticationRedirector.java
+++ src/main/java/org/fenixedu/ulisboa/specifications/authentication/ULisboaAuthenticationRedirector.java
@@ -5,36 +5,40 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.fenixedu.bennu.core.domain.User;
-import org.fenixedu.bennu.portal.domain.PortalConfiguration;
+import org.fenixedu.bennu.core.security.Authenticate;
 
 import com.google.common.collect.Lists;
 
 public class ULisboaAuthenticationRedirector {
 
     private static List<IULisboaRedirectionHandler> handlers = Lists.newArrayList();
-    
+
     public static void registerRedirectionHandler(final IULisboaRedirectionHandler handler) {
-        synchronized(handlers) {
+        synchronized (handlers) {
             handlers.add(handler);
         }
     }
-    
+
     public static void unregisterRedirectionHandler(final IULisboaRedirectionHandler handler) {
-        synchronized(handlers) {
+        synchronized (handlers) {
             handlers.remove(handler);
         }
     }
-    
+
+    public static String getRedirectionPath(final HttpServletRequest request) {
+        return getRedirectionPath(Authenticate.getUser(), request);
+    }
+
     public static String getRedirectionPath(final User user, final HttpServletRequest request) {
-        synchronized(handlers) {
+        synchronized (handlers) {
             for (final IULisboaRedirectionHandler iuLisboaRedirectionHandler : handlers) {
-                if(iuLisboaRedirectionHandler.isToRedirect(user, request)) {
+                if (iuLisboaRedirectionHandler.isToRedirect(user, request)) {
                     return iuLisboaRedirectionHandler.redirectionPath(user, request);
                 }
             }
         }
-        
-        return PortalConfiguration.getInstance().getMenu().getUserMenuStream().findFirst().get().getPath();
+
+        return null;
     }
-    
+
 }
