0ee2c3b7f1e43645a0ad694a45a34978d80413e7
Nadir Amin <nadir.amin@qub-it.com>
2016-04-26 14:03:03 +0100

    Migrations WIP

--- src/main/java/org/fenixedu/ulisboa/specifications/domain/services/CurriculumLineServices.java
+++ src/main/java/org/fenixedu/ulisboa/specifications/domain/services/CurriculumLineServices.java
@@ -1,5 +1,7 @@
 package org.fenixedu.ulisboa.specifications.domain.services;
 
+import java.math.BigDecimal;
+
 import org.fenixedu.academic.domain.studentCurriculum.CurriculumLine;
 import org.fenixedu.ulisboa.specifications.domain.studentCurriculum.CurriculumLineExtendedInformation;
 
@@ -20,6 +22,32 @@ public class CurriculumLineServices {
     static public Integer getCurricularYear(final CurriculumLine curriculumLine) {
         return curriculumLine.getExtendedInformation() == null ? null : curriculumLine.getExtendedInformation()
                 .getCurricularYear();
+    }
+
+    static public void setExcludedFromAverage(CurriculumLine curriculumLine, Boolean excludedFromAverage) {
+        CurriculumLineExtendedInformation.findOrCreate(curriculumLine).setExcludedFromAverage(excludedFromAverage);
+    }
+
+    static public Boolean isExcludedFromAverage(CurriculumLine curriculumLine) {
+        return curriculumLine.getExtendedInformation() != null
+                && curriculumLine.getExtendedInformation().getExcludedFromAverage() != null
+                && curriculumLine.getExtendedInformation().getExcludedFromAverage().booleanValue();
+    }
+
+    static public void setEctsCredits(CurriculumLine curriculumLine, BigDecimal ectsCredits) {
+        CurriculumLineExtendedInformation.findOrCreate(curriculumLine).setEctsCredits(ectsCredits);
+    }
+
+    static public BigDecimal getEctsCredits(CurriculumLine curriculumLine) {
+        return curriculumLine.getExtendedInformation() == null ? null : curriculumLine.getExtendedInformation().getEctsCredits();
+    }
+
+    static public void setWeight(CurriculumLine curriculumLine, BigDecimal weight) {
+        CurriculumLineExtendedInformation.findOrCreate(curriculumLine).setWeight(weight);
+    }
+
+    static public BigDecimal getWeight(CurriculumLine curriculumLine) {
+        return curriculumLine.getExtendedInformation() == null ? null : curriculumLine.getExtendedInformation().getWeight();
     }
 
 }
---
+++ src/main/java/org/fenixedu/ulisboa/specifications/domain/services/PersonServices.java
@@ -0,0 +1,16 @@
+package org.fenixedu.ulisboa.specifications.domain.services;
+
+import org.fenixedu.academic.domain.Person;
+import org.fenixedu.ulisboa.specifications.domain.PersonUlisboaSpecifications;
+
+public class PersonServices {
+
+    static public void setPersonnelNumber(final Person person, final String number) {
+        PersonUlisboaSpecifications.findOrCreate(person).setPersonnelNumber(number);
+    }
+
+    static public String getPersonnelNumber(final Person person) {
+        return person.getPersonUlisboaSpecifications() == null ? null : person.getPersonUlisboaSpecifications()
+                .getPersonnelNumber();
+    }
+}
