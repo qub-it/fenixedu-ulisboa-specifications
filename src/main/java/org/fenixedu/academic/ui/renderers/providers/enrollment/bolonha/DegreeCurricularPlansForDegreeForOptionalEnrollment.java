6a5ab6e2c53544e6dd3bf911e1eb36c54b6cce2a
Luís Egídio <luis.egidio@gmail.com>
2016-06-01 10:57:59 +0100

    [Optional Enrolment UI] - Refactor: AnyCurricularCourseExceptions dealt with on renderer/layout level instead of JavaScript voodoo

--- src/main/java/org/fenixedu/academic/ui/renderers/providers/enrollment/bolonha/DegreeCurricularPlansForDegreeForOptionalEnrollment.java
+++ src/main/java/org/fenixedu/academic/ui/renderers/providers/enrollment/bolonha/DegreeCurricularPlansForDegreeForOptionalEnrollment.java
@@ -4,14 +4,10 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
-import org.fenixedu.academic.domain.CompetenceCourse;
-import org.fenixedu.academic.domain.CurricularCourse;
 import org.fenixedu.academic.domain.Degree;
 import org.fenixedu.academic.domain.DegreeCurricularPlan;
-import org.fenixedu.academic.domain.curricularRules.executors.ruleExecutors.AnyCurricularCourseExceptionsExecutorLogic;
 import org.fenixedu.academic.domain.degree.DegreeType;
 import org.fenixedu.academic.dto.student.enrollment.bolonha.BolonhaStudentOptionalEnrollmentBean;
-import org.fenixedu.ulisboa.specifications.domain.curricularRules.AnyCurricularCourseExceptionsConfiguration;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
@@ -53,32 +49,6 @@ public class DegreeCurricularPlansForDegreeForOptionalEnrollment extends DegreeC
                 for (final DegreeCurricularPlan iter : degree.getDegreeCurricularPlansSet()) {
                     if (iter.isActive() && iter.hasExecutionDegreeFor(bean.getExecutionYear())) {
                         result.add(iter);
-                    }
-                }
-            }
-        }
-
-        return result;
-    }
-
-    static public Set<String> getAnyCurricularCourseExceptionsOIDs(final BolonhaStudentOptionalEnrollmentBean bean) {
-        final Set<String> result = Sets.newHashSet();
-
-        final AnyCurricularCourseExceptionsConfiguration configuration = AnyCurricularCourseExceptionsConfiguration.getInstance();
-        final DegreeCurricularPlan chosenDegreeCurricularPlan = bean.getDegreeCurricularPlan();
-
-        if (chosenDegreeCurricularPlan != null && configuration != null) {
-
-            for (final CompetenceCourse competenceCourse : configuration.getCompetenceCoursesSet()) {
-
-                // just to avoid a JSP with an array with 500 OIDs
-                final CurricularCourse curricularCourse = competenceCourse.getCurricularCourse(chosenDegreeCurricularPlan);
-                if (curricularCourse != null) {
-
-                    if (AnyCurricularCourseExceptionsExecutorLogic.isException(competenceCourse, chosenDegreeCurricularPlan,
-                            bean.getStudentCurricularPlan())) {
-
-                        result.add(curricularCourse.getExternalId());
                     }
                 }
             }
