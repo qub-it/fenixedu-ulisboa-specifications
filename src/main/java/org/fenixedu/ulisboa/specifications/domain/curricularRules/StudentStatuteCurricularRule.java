6e082a12c4a716cc1ba26c5748ff2fa9acdec896
Luís Egídio <luis.egidio@gmail.com>
2016-07-16 06:53:53 +0100

    WIP StudentSchoolClass and StudentStatute CRs

---
+++ src/main/java/org/fenixedu/ulisboa/specifications/domain/curricularRules/StudentStatuteCurricularRule.java
@@ -0,0 +1,66 @@
+package org.fenixedu.ulisboa.specifications.domain.curricularRules;
+
+import java.util.List;
+
+import org.fenixedu.academic.domain.ExecutionSemester;
+import org.fenixedu.academic.domain.curricularRules.CurricularRuleType;
+import org.fenixedu.academic.domain.curricularRules.executors.RuleResult;
+import org.fenixedu.academic.domain.curricularRules.executors.verifyExecutors.VerifyRuleExecutor;
+import org.fenixedu.academic.domain.degreeStructure.CourseGroup;
+import org.fenixedu.academic.domain.degreeStructure.DegreeModule;
+import org.fenixedu.academic.domain.enrolment.EnrolmentContext;
+import org.fenixedu.academic.domain.enrolment.IDegreeModuleToEvaluate;
+import org.fenixedu.academic.dto.GenericPair;
+import org.fenixedu.bennu.core.i18n.BundleUtil;
+import org.fenixedu.ulisboa.specifications.domain.curricularRules.executors.ruleExecutors.StudentStatuteCurricularRuleExecutor;
+import org.fenixedu.ulisboa.specifications.util.ULisboaConstants;
+
+import com.google.common.collect.Lists;
+
+public class StudentStatuteCurricularRule extends StudentStatuteCurricularRule_Base {
+
+    protected StudentStatuteCurricularRule() {
+        super();
+    }
+
+    public StudentStatuteCurricularRule(final DegreeModule toApplyRule, final CourseGroup contextCourseGroup,
+            final ExecutionSemester begin, final ExecutionSemester end) {
+
+        this();
+        init(toApplyRule, contextCourseGroup, begin, end, CurricularRuleType.CUSTOM);
+    }
+
+    public void edit(CourseGroup contextCourseGroup) {
+        setContextCourseGroup(contextCourseGroup);
+    }
+
+    @Override
+    public RuleResult evaluate(IDegreeModuleToEvaluate sourceDegreeModuleToEvaluate, EnrolmentContext enrolmentContext) {
+        return new StudentStatuteCurricularRuleExecutor().execute(this, sourceDegreeModuleToEvaluate, enrolmentContext);
+    }
+
+    @Override
+    public VerifyRuleExecutor createVerifyRuleExecutor() {
+        return VerifyRuleExecutor.NULL_VERIFY_EXECUTOR;
+    }
+
+    @Override
+    protected void removeOwnParameters() {
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<GenericPair<Object, Boolean>> getLabel() {
+        final StringBuilder label = new StringBuilder();
+        label.append(BundleUtil.getString(ULisboaConstants.BUNDLE, "label.StudentStatuteCurricularRule"));
+        if (getContextCourseGroup() != null) {
+            label.append(", ");
+            label.append(BundleUtil.getString(ULisboaConstants.BUNDLE, "label.inGroup"));
+            label.append(" ");
+            label.append(getContextCourseGroup().getOneFullName());
+        }
+
+        return Lists.newArrayList(new GenericPair<Object, Boolean>(label, false));
+    }
+
+}
---
+++ src/main/java/org/fenixedu/ulisboa/specifications/domain/curricularRules/executors/ruleExecutors/StudentSchoolClassCurricularRuleExecutor.java
@@ -0,0 +1,46 @@
+package org.fenixedu.ulisboa.specifications.domain.curricularRules.executors.ruleExecutors;
+
+import org.fenixedu.academic.domain.curricularRules.ICurricularRule;
+import org.fenixedu.academic.domain.curricularRules.executors.RuleResult;
+import org.fenixedu.academic.domain.curricularRules.executors.ruleExecutors.CurricularRuleExecutor;
+import org.fenixedu.academic.domain.enrolment.EnrolmentContext;
+import org.fenixedu.academic.domain.enrolment.IDegreeModuleToEvaluate;
+
+public class StudentSchoolClassCurricularRuleExecutor extends CurricularRuleExecutor {
+
+    @Override
+    protected RuleResult executeEnrolmentInEnrolmentEvaluation(final ICurricularRule curricularRule,
+            final IDegreeModuleToEvaluate sourceDegreeModuleToEvaluate, final EnrolmentContext enrolmentContext) {
+        return RuleResult.createNA(sourceDegreeModuleToEvaluate.getDegreeModule());
+    }
+
+    @Override
+    protected boolean canBeEvaluated(ICurricularRule curricularRule, IDegreeModuleToEvaluate sourceDegreeModuleToEvaluate,
+            EnrolmentContext enrolmentContext) {
+        return true;
+    }
+
+    // author = "legidio", comment = "Deprecated: grades must be set before enrolment periods"
+    @Override
+    @Deprecated
+    protected RuleResult executeEnrolmentWithRulesAndTemporaryEnrolment(final ICurricularRule curricularRule,
+            IDegreeModuleToEvaluate sourceDegreeModuleToEvaluate, final EnrolmentContext enrolmentContext) {
+        return executeEnrolmentVerificationWithRules(curricularRule, sourceDegreeModuleToEvaluate, enrolmentContext);
+    }
+
+    @Override
+    protected RuleResult executeEnrolmentVerificationWithRules(ICurricularRule curricularRule,
+            IDegreeModuleToEvaluate sourceDegreeModuleToEvaluate, EnrolmentContext enrolmentContext) {
+
+        if (!canApplyRule(enrolmentContext, curricularRule)) {
+            return RuleResult.createNA(sourceDegreeModuleToEvaluate.getDegreeModule());
+        }
+
+        final IDegreeModuleToEvaluate degreeModuleToEvaluate = searchDegreeModuleToEvaluate(enrolmentContext, curricularRule);
+
+        // TODO N+L
+        return RuleResult.createFalse(sourceDegreeModuleToEvaluate.getDegreeModule(),
+                "curricularRules.ruleExecutors.StudentSchoolClassCurricularRuleExecutor.error.example");
+    }
+
+}
