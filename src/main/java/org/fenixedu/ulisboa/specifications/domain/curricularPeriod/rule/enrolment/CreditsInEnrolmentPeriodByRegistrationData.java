fd0003eb152c1bfb67a93d27ad4844bb807feacd
Luís Egídio <luis.egidio@gmail.com>
2016-09-13 19:04:01 +0100

    [Enrolment Rule] New Feature: CreditsInEnrolmentPeriodByRegistrationData

---
+++ src/main/java/org/fenixedu/ulisboa/specifications/domain/curricularPeriod/rule/enrolment/CreditsInEnrolmentPeriodByRegistrationData.java
@@ -0,0 +1,47 @@
+package org.fenixedu.ulisboa.specifications.domain.curricularPeriod.rule.enrolment;
+
+import java.math.BigDecimal;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.fenixedu.academic.domain.curricularRules.executors.RuleResult;
+import org.fenixedu.academic.domain.enrolment.EnrolmentContext;
+import org.fenixedu.academic.domain.student.Registration;
+import org.fenixedu.academic.domain.student.RegistrationDataByExecutionYear;
+import org.fenixedu.ulisboa.specifications.domain.curricularPeriod.CurricularPeriodConfiguration;
+
+import pt.ist.fenixframework.Atomic;
+
+public class CreditsInEnrolmentPeriodByRegistrationData extends CreditsInEnrolmentPeriodByRegistrationData_Base {
+
+    protected CreditsInEnrolmentPeriodByRegistrationData() {
+        super();
+    }
+
+    @Atomic
+    static public CreditsInEnrolmentPeriodByRegistrationData create(final CurricularPeriodConfiguration configuration,
+            final BigDecimal credits, final Integer semester, final Integer year) {
+
+        final CreditsInEnrolmentPeriodByRegistrationData result = new CreditsInEnrolmentPeriodByRegistrationData();
+        result.init(configuration, credits, semester);
+        result.setYearMin(year);
+        result.setYearMax(year);
+
+        return result;
+    }
+
+    @Override
+    public RuleResult execute(final EnrolmentContext enrolmentContext) {
+
+        final Registration registration = enrolmentContext.getRegistration();
+
+        final Set<RegistrationDataByExecutionYear> datas = registration.getRegistrationDataByExecutionYearSet().stream()
+                .filter(i -> i.getEnrolmentDate() != null).collect(Collectors.toSet());
+        if (!datas.isEmpty() && datas.size() > 1) {
+            return createNA();
+        }
+
+        return super.execute(enrolmentContext);
+    }
+
+}
