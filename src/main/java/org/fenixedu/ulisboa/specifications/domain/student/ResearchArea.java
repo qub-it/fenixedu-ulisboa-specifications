aac7248d49c833f57ac7b4bd644fefd5c0784f20
Gilberto Pedrosa <gilberto.pedrosa@qub-it.com>
2018-06-19 16:03:03 +0100

    Improved ResearchArea class - added findByCode() and findAll() methods

--- src/main/java/org/fenixedu/ulisboa/specifications/domain/student/ResearchArea.java
+++ src/main/java/org/fenixedu/ulisboa/specifications/domain/student/ResearchArea.java
@@ -1,12 +1,16 @@
 package org.fenixedu.ulisboa.specifications.domain.student;
 
+import java.util.Collection;
 import java.util.Comparator;
+import java.util.Objects;
 
+import org.fenixedu.academic.domain.exceptions.DomainException;
 import org.fenixedu.bennu.core.domain.Bennu;
 import org.fenixedu.commons.i18n.LocalizedString;
+import org.fenixedu.ulisboa.specifications.domain.exceptions.ULisboaSpecificationsDomainException;
 
 public class ResearchArea extends ResearchArea_Base {
-    
+
     public static final Comparator<ResearchArea> COMPARATOR_BY_CODE = new Comparator<ResearchArea>() {
 
         @Override
@@ -15,20 +19,38 @@ public class ResearchArea extends ResearchArea_Base {
             return c != 0 ? c : o1.getExternalId().compareTo(o2.getExternalId());
         }
     };
-    
+
     protected ResearchArea() {
         super();
         setBennu(Bennu.getInstance());
     }
-    
+
     protected ResearchArea(final String code, final LocalizedString name) {
         this();
         setCode(code);
         setName(name);
+
+        checkRules();
     }
-    
+
+    private void checkRules() {
+
+        if (findAll().stream().anyMatch(r -> r != this && Objects.equals(r.getCode(), getCode()))) {
+            throw new ULisboaSpecificationsDomainException("error.ResearchArea.already.exists.with.same.code");
+        }
+
+    }
+
     public static final ResearchArea create(final String code, final LocalizedString name) {
         return new ResearchArea(code, name);
     }
-    
+
+    public static ResearchArea findByCode(String code) {
+        return findAll().stream().filter(fos -> code.equalsIgnoreCase(fos.getCode())).findFirst().orElse(null);
+    }
+
+    public static Collection<ResearchArea> findAll() {
+        return Bennu.getInstance().getResearchAreasSet();
+    }
+
 }
