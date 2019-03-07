eecd73215e58f1be220ff4df242f5a1029c3a3f9
Shezad Anavarali <shezad.anavarali@qub-it.com>
2019-03-07 15:16:42 +0000

    Simplified periods provider for backoffice enrolments

--- src/main/java/org/fenixedu/academic/ui/renderers/providers/enrollment/bolonha/ExecutionPeriodsForEnrolmentProvider.java
+++ src/main/java/org/fenixedu/academic/ui/renderers/providers/enrollment/bolonha/ExecutionPeriodsForEnrolmentProvider.java
@@ -27,19 +27,13 @@ package org.fenixedu.academic.ui.renderers.providers.enrollment.bolonha;
 
 import java.util.Collections;
 import java.util.Set;
-import java.util.SortedSet;
 import java.util.TreeSet;
 
-import org.fenixedu.academic.domain.DegreeCurricularPlan;
 import org.fenixedu.academic.domain.ExecutionSemester;
 import org.fenixedu.academic.domain.ExecutionYear;
 import org.fenixedu.academic.domain.StudentCurricularPlan;
-import org.fenixedu.academic.domain.degreeStructure.Context;
-import org.fenixedu.academic.domain.degreeStructure.CourseGroup;
 import org.fenixedu.academic.dto.student.IStudentCurricularPlanBean;
 import org.fenixedu.academic.ui.renderers.providers.ExecutionPeriodsForDismissalsStudentCurricularPlanProvider;
-
-import com.google.common.collect.Sets;
 
 import pt.ist.fenixWebFramework.rendererExtensions.converters.DomainObjectKeyConverter;
 import pt.ist.fenixWebFramework.renderers.DataProvider;
@@ -62,74 +56,14 @@ public class ExecutionPeriodsForEnrolmentProvider implements DataProvider {
 
         final StudentCurricularPlan studentCurricularPlan = ((IStudentCurricularPlanBean) source).getStudentCurricularPlan();
 
-        final ExecutionSemester beginInterval = getFirstExecutionYear(studentCurricularPlan).getFirstExecutionPeriod();
-        final ExecutionSemester endInterval = getLastExecutionYear(studentCurricularPlan).getLastExecutionPeriod();
-
-        final SortedSet<ExecutionSemester> result = new TreeSet<ExecutionSemester>(Collections.reverseOrder());
-
-        ExecutionSemester interval = beginInterval;
-        while (interval != null && interval.isBeforeOrEquals(endInterval)) {
-            result.add(interval);
-            interval = interval.getNextExecutionPeriod();
-        } 
-
-        return result;
-    }
-
-    static private ExecutionYear getFirstExecutionYear(final StudentCurricularPlan plan) {
-        final SortedSet<ExecutionYear> result = Sets.newTreeSet(ExecutionYear.COMPARATOR_BY_YEAR);
-
-        // should be enough, if it wasn't for wrong data
-        result.add(ExecutionYear.readByDateTime(plan.getStartDateYearMonthDay().toDateTimeAtCurrentTime()));
-
-        // whatever the case, the SCP lines must be able to be accessible
-        final ExecutionSemester firstSemester = plan.getFirstExecutionPeriod();
-        final ExecutionYear firstScpYear = firstSemester == null ? null : firstSemester.getExecutionYear();
-        if (firstScpYear != null) {
-            result.add(firstScpYear);
+        final Set<ExecutionSemester> result = new TreeSet<ExecutionSemester>(Collections.reverseOrder());
+        ExecutionYear executionYear = studentCurricularPlan.getStartExecutionYear();
+        result.addAll(executionYear.getExecutionPeriodsSet());
+        while (executionYear.getNextExecutionYear() != null) {
+            result.addAll(executionYear.getNextExecutionYear().getExecutionPeriodsSet());
+            executionYear = executionYear.getNextExecutionYear();
         }
 
-        return result.first();
-    }
-
-    static private ExecutionYear getLastExecutionYear(final StudentCurricularPlan plan) {
-        final SortedSet<ExecutionYear> result = Sets.newTreeSet(ExecutionYear.COMPARATOR_BY_YEAR);
-
-        // whatever the case, the SCP lines must be able to be accessible 
-        final ExecutionYear lastScpYear = plan.getLastExecutionYear();
-        if (lastScpYear != null) {
-            result.add(lastScpYear);
-        }
-
-        // inspect DCP executions
-        final DegreeCurricularPlan dcp = plan.getDegreeCurricularPlan();
-        if (!dcp.getExecutionDegreesSet().isEmpty()) {
-            result.add(dcp.getLastExecutionYear());
-        }
-
-        // inspect DCP definition
-        result.addAll(getEndContextExecutionYears(dcp.getRoot()));
-
-        return result.last();
-    }
-
-    static public Set<ExecutionYear> getEndContextExecutionYears(final CourseGroup input) {
-        final Set<ExecutionYear> result = Sets.newHashSet();
-        for (final Context context : input.getChildContexts(CourseGroup.class)) {
-            final ExecutionSemester endSemester = context.getEndExecutionPeriod();
-            if (endSemester != null) {
-                result.add(endSemester.getExecutionYear());
-            } else {
-                final ExecutionYear currentExecutionYear = ExecutionYear.readCurrentExecutionYear();
-                result.add(currentExecutionYear);
-
-                final ExecutionYear nextExecutionYear = currentExecutionYear.getNextExecutionYear();
-                if (nextExecutionYear != null) {
-                    result.add(nextExecutionYear);
-                }
-            }
-            result.addAll(getEndContextExecutionYears((CourseGroup) context.getChildDegreeModule()));
-        }
         return result;
     }
 
