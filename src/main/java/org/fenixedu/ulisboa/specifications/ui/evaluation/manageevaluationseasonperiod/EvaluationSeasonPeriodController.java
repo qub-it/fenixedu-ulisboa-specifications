6ba835377324b40772cf9083f8df3ef276f57fb6
Shezad Anavarali <shezad.anavarali@qub-it.com>
2022-09-21 15:42:42 +0100

    Refactor: Replace usage of deprecated evaluation season periods API

--- src/main/java/org/fenixedu/ulisboa/specifications/ui/evaluation/manageevaluationseasonperiod/EvaluationSeasonPeriodController.java
+++ src/main/java/org/fenixedu/ulisboa/specifications/ui/evaluation/manageevaluationseasonperiod/EvaluationSeasonPeriodController.java
@@ -116,7 +116,7 @@ public class EvaluationSeasonPeriodController extends FenixeduUlisboaSpecificati
 
     private Stream<EvaluationSeasonPeriod> getSearchUniverseSearchDataSet(final ExecutionYear executionYear,
             final EvaluationSeasonPeriodType periodType) {
-        return EvaluationSeasonPeriod.findBy(executionYear, periodType).stream();
+        return EvaluationSeasonPeriod.findBy(executionYear).stream().filter(p -> p.getPeriodType() == periodType);
     }
 
     private List<EvaluationSeasonPeriod> filterSearch(final ExecutionYear executionYear,
