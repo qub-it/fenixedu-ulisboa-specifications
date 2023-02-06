7258627485273984537c4a641a86442bf5d5f3b9
Shezad Anavarali <shezad.anavarali@qub-it.com>
2023-02-06 14:03:58 +0000

    Refactor: Add missing Department methods in order to mantain compability

--- src/main/java/org/fenixedu/academic/domain/Department.java
+++ src/main/java/org/fenixedu/academic/domain/Department.java
@@ -1,9 +1,16 @@
 package org.fenixedu.academic.domain;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
+
 public class Department extends Department_Base {
-    
+
     public Department() {
         super();
     }
-    
+
+    public String getAcronym() {
+        return StringUtils.isNotBlank(getCode()) ? getCode() : WordUtils.initials(getName()).replaceAll("[a-z]", "");
+    }
+
 }
