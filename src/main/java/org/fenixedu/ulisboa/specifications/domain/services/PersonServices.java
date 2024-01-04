30ed1d4dc47b4750d68d0bfd0c35079b82b15cf0
Shezad Anavarali <shezad.anavarali@qub-it.com>
2024-01-04 11:42:32 +0000

    Refactor: PersonServices not extending academic-extensions service
    Issue: #qubIT-Fenix-4784

--- src/main/java/org/fenixedu/ulisboa/specifications/domain/services/PersonServices.java
+++ src/main/java/org/fenixedu/ulisboa/specifications/domain/services/PersonServices.java
@@ -3,7 +3,7 @@ package org.fenixedu.ulisboa.specifications.domain.services;
 import org.fenixedu.academic.domain.Person;
 import org.fenixedu.ulisboa.specifications.domain.PersonUlisboaSpecifications;
 
-public class PersonServices extends org.fenixedu.academic.domain.person.services.PersonServices {
+public class PersonServices {
 
     static public void setPersonnelNumber(final Person person, final String number) {
         PersonUlisboaSpecifications.findOrCreate(person).setPersonnelNumber(number);
@@ -12,5 +12,19 @@ public class PersonServices extends org.fenixedu.academic.domain.person.services
     static public String getPersonnelNumber(final Person person) {
         return person.getPersonUlisboaSpecifications() == null ? null : person.getPersonUlisboaSpecifications()
                 .getPersonnelNumber();
+    }
+
+    static public String getDisplayName(final Person input) {
+        String result = "";
+
+        if (input != null && input.getProfile() != null) {
+            result = input.getProfile().getDisplayName();
+
+            if (result.equals(input.getName()) || !result.trim().contains(" ")) {
+                result = input.getFirstAndLastName();
+            }
+        }
+
+        return result;
     }
 }
