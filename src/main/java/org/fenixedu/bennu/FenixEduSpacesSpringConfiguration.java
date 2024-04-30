d61bb46d2d44bbed8fd218f3e7acc28f71619bb2
Paulo Abrantes <paulo.abrantes@qub-it.com>
2024-04-30 11:19:28 +0100

    Adds class with @BennuSpringModule for spaces
    Spring controllers are only registered in bennu when there's a @BennuSpringModule
    that identifies the package of the controller in the basePackages.
    
    Since we are removing bennu-spring from spaces, I'm moving the class
    into specifications. Creating a new class instead of having multiple
    packages defined in FenixeduUlisboaSpecificationsSpringConfiguration so
    we can see in the UI two different applications.
    
    Relates with: #qubIT-Omnis-5811

---
+++ src/main/java/org/fenixedu/bennu/FenixEduSpacesSpringConfiguration.java
@@ -0,0 +1,8 @@
+package org.fenixedu.bennu;
+
+import org.fenixedu.bennu.spring.BennuSpringModule;
+
+@BennuSpringModule(basePackages = "org.fenixedu.spaces.ui", bundles = "SpacesResources")
+public class FenixEduSpacesSpringConfiguration {
+
+}
