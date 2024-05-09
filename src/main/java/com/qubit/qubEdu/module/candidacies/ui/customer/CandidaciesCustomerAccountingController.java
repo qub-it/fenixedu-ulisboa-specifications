b3f40ca0868adbba45c0b9a3a8e042872df8405b
Anil Mamede <anil.mamede@qub-it.com>
2024-05-09 09:43:13 +0100

    - Refactor: Remove license from refactored classes from other module

--- src/main/java/com/qubit/qubEdu/module/candidacies/ui/customer/CandidaciesCustomerAccountingController.java
+++ src/main/java/com/qubit/qubEdu/module/candidacies/ui/customer/CandidaciesCustomerAccountingController.java
@@ -1,79 +1,7 @@
-/**
- * OMNIS.CLOUD SOFTWARE LICENSE
- *
- * Copyright (C) 2008-2021 Quorum Born IT <https://www.qub-it.com>
- * All rights reserved.
- *
- * 1. This software and all its associated and derivative rights belong
- * to Quorum Born IT (Quorum Born IT).
- *
- * 2. Copying, redistribution and use of this software, in source code
- * and/or binary forms are exclusive to Quorum Born IT and/or its partners.
- * The terms of the agreements between Quorum Born IT and its partners must
- * be defined in a written and valid partnership agreement, signed by both
- * entities.
- *
- * 3. Commercial use in source code and/or binary forms are exclusive to
- * Quorum Born IT, being licensed and not sold.
- *
- * 4. Quorum Born IT may grant a limited and controlled license to other
- * entities to use this software, following established partner agreements,
- * but commercial exploration and/or resale and/or sublicensing are not
- * allowed in any circunstances.
- *
- * 5. Quorum Born IT might authorize read-only access to this software in
- * the context of non-commercial use and/or within academic research and/or
- * educational contexts, provided that the following conditions are met:
- * 	a) All the individuals that will access the software must receive an
- * 	explicit authorization for it by Quorum Born IT, configured via
- * 	source-control system or similar mechanisms.
- * 	b) Quorum Born IT must be informed of the dates to end the access.
- * 	c) The software, being in source code and/or binary forms, is not
- * 	to be incorporated into proprietary software (unless belonging to
- * 	Quorum Born IT).
- *
- * 6. Modifications to this software by Quorum Born IT partners are permitted
- * provided that the following conditions are met:
- * 	a) Redistributions of source code must retain this license file.
- * 	b) Redistributions in binary form must reproduce this license file
- * 	in the documentation and/or other materials within the correspondent
- * 	distribution package.
- * 	c) Submitting back to Quorum Born IT all the modified versions of this
- * 	software source code.
- * 	d) Never to modify any binary forms of this software.
- * 	e) Unless it is Quorum Born IT incorporating received source code
- * 	modifications of this software in its base version, followed it with
- * 	the associated release, there is an explicit renounce to any Quorum
- * 	Born IT liability regarding the proper functioning of this software
- * 	and/or other software with which it interacts and/or integrates,
- * 	when modified by entities other than Quorum Born IT.
- *
- * 7. Activities involving the manipulation of this software not described in
- * this license, being in source code and/or binary forms, are outside its
- * scope and therefore are not allowed.
- *
- * 8. This software may not be error free, being licensed "as is" and
- * Quorum Born IT gives no express warranties, guarantees or conditions.
- * 	a) To the extent permitted under applicable laws, Quorum Born IT
- * 	excludes all implied warranties, including merchantability, fitness
- * 	for a particular purpose, and non-infringement.
- * 	b) Quorum Born IT shall not be liable for any lost profits, lost
- * 	revenues, lost opportunities, downtime, or any consequential damages
- * 	or costs, resulting from any claim or cause of action based on breach
- * 	of warranty, breach of contract, negligence, or any other legal theory,
- * 	that arises from the use of this software by others.
- *
- * 9. Everyone is permitted to copy and distribute verbatim copies of this
- * license document, but changes to it are not allowed.
- *
- *
- * Last updated: 13 July 2021
- */
 package com.qubit.qubEdu.module.candidacies.ui.customer;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.fenixedu.academictreasury.ui.customer.forwardpayments.CustomerAccountingForwardPaymentController;
 import org.fenixedu.bennu.spring.portal.SpringFunctionality;
 import org.fenixedu.treasury.domain.debt.DebtAccount;
 import org.fenixedu.treasury.domain.document.FinantialDocument;
@@ -115,20 +43,21 @@ public class CandidaciesCustomerAccountingController
     protected String getForwardPaymentUrl(DebtAccount debtAccount, DigitalPaymentPlatform digitalPaymentPlatform) {
         return FORWARD_PAYMENT_URL(debtAccount, digitalPaymentPlatform);
     }
-    
+
     public static String FORWARD_PAYMENT_URL(DebtAccount debtAccount, DigitalPaymentPlatform digitalPaymentPlatform) {
-        return String.format(CONTROLLER_URL + "/read/%s/forwardpayment/%s", debtAccount.getExternalId(), digitalPaymentPlatform.getExternalId());
+        return String.format(CONTROLLER_URL + "/read/%s/forwardpayment/%s", debtAccount.getExternalId(),
+                digitalPaymentPlatform.getExternalId());
     }
-    
+
     @Override
     protected String getPrintSettlementNote() {
         return PRINT_SETTLEMENT_NOTE_URL;
     }
-    
+
     protected String getDownloadCertifiedDocumentPrintUrl() {
         return DOWNLOAD_CERTIFIED_DOCUMENT_PRINT_URL;
     }
-    
+
     @RequestMapping
     @Override
     public String home(Model model) {
@@ -143,21 +72,19 @@ public class CandidaciesCustomerAccountingController
 
     @RequestMapping(value = READ_ACCOUNT_URI + "{oid}")
     @Override
-    public String readAccount(@PathVariable(value = "oid") DebtAccount debtAccount, Model model, final RedirectAttributes redirectAttributes) {
+    public String readAccount(@PathVariable(value = "oid") DebtAccount debtAccount, Model model,
+            final RedirectAttributes redirectAttributes) {
         return super.readAccount(debtAccount, model, redirectAttributes);
     }
 
     @RequestMapping(value = "/read/{oid}/forwardpayment/{digitalPaymentPlatformId}")
-    public String processReadToForwardPayment(
-            @PathVariable("oid") DebtAccount debtAccount,
-            @PathVariable("digitalPaymentPlatformId") DigitalPaymentPlatform digitalPaymentPlatform,
-            Model model,
+    public String processReadToForwardPayment(@PathVariable("oid") DebtAccount debtAccount,
+            @PathVariable("digitalPaymentPlatformId") DigitalPaymentPlatform digitalPaymentPlatform, Model model,
             RedirectAttributes redirectAttributes) {
-        return redirect(CandidaciesCustomerAccountingForwardPaymentController.CHOOSE_INVOICE_ENTRIES_URL + debtAccount.getExternalId() +
-                "/" + digitalPaymentPlatform.getExternalId(),
-                model, redirectAttributes);
+        return redirect(CandidaciesCustomerAccountingForwardPaymentController.CHOOSE_INVOICE_ENTRIES_URL
+                + debtAccount.getExternalId() + "/" + digitalPaymentPlatform.getExternalId(), model, redirectAttributes);
     }
-    
+
     private static final String CUSTOMER_NOT_CREATED_URI = "/customernotcreated";
     public static final String CUSTOMER_NOT_CREATED_URL = CONTROLLER_URL + CUSTOMER_NOT_CREATED_URI;
 
@@ -165,12 +92,12 @@ public class CandidaciesCustomerAccountingController
     protected String getCustomerNotCreatedUrl() {
         return CUSTOMER_NOT_CREATED_URL;
     }
-    
+
     @RequestMapping(value = CUSTOMER_NOT_CREATED_URI)
     public String customernotcreated(final Model model) {
         return super.customernotcreated(model);
     }
-    
+
     private static final String PRINT_SETTLEMENT_NOTE_URI = "/printsettlementnote";
     public static final String PRINT_SETTLEMENT_NOTE_URL = CONTROLLER_URL + PRINT_SETTLEMENT_NOTE_URI;
 
@@ -181,7 +108,7 @@ public class CandidaciesCustomerAccountingController
             final RedirectAttributes redirectAttributes) {
         return super.printsettlementnote(settlementNote, model, redirectAttributes);
     }
-    
+
     private static final String _DOWNLOAD_CERTIFIED_DOCUMENT_PRINT_URI = "/downloadcertifieddocumentprint";
     public static final String DOWNLOAD_CERTIFIED_DOCUMENT_PRINT_URL = CONTROLLER_URL + _DOWNLOAD_CERTIFIED_DOCUMENT_PRINT_URI;
 
@@ -191,5 +118,4 @@ public class CandidaciesCustomerAccountingController
         return super.downloadcertifieddocumentprint(finantialDocument, model, redirectAttributes, response);
     }
 
-    
 }
--- src/main/java/com/qubit/qubEdu/module/candidacies/ui/customer/forwardpayments/CandidaciesCustomerAccountingForwardPaymentController.java
+++ src/main/java/com/qubit/qubEdu/module/candidacies/ui/customer/forwardpayments/CandidaciesCustomerAccountingForwardPaymentController.java
@@ -1,74 +1,3 @@
-/**
- * OMNIS.CLOUD SOFTWARE LICENSE
- *
- * Copyright (C) 2008-2021 Quorum Born IT <https://www.qub-it.com>
- * All rights reserved.
- *
- * 1. This software and all its associated and derivative rights belong
- * to Quorum Born IT (Quorum Born IT).
- *
- * 2. Copying, redistribution and use of this software, in source code
- * and/or binary forms are exclusive to Quorum Born IT and/or its partners.
- * The terms of the agreements between Quorum Born IT and its partners must
- * be defined in a written and valid partnership agreement, signed by both
- * entities.
- *
- * 3. Commercial use in source code and/or binary forms are exclusive to
- * Quorum Born IT, being licensed and not sold.
- *
- * 4. Quorum Born IT may grant a limited and controlled license to other
- * entities to use this software, following established partner agreements,
- * but commercial exploration and/or resale and/or sublicensing are not
- * allowed in any circunstances.
- *
- * 5. Quorum Born IT might authorize read-only access to this software in
- * the context of non-commercial use and/or within academic research and/or
- * educational contexts, provided that the following conditions are met:
- * 	a) All the individuals that will access the software must receive an
- * 	explicit authorization for it by Quorum Born IT, configured via
- * 	source-control system or similar mechanisms.
- * 	b) Quorum Born IT must be informed of the dates to end the access.
- * 	c) The software, being in source code and/or binary forms, is not
- * 	to be incorporated into proprietary software (unless belonging to
- * 	Quorum Born IT).
- *
- * 6. Modifications to this software by Quorum Born IT partners are permitted
- * provided that the following conditions are met:
- * 	a) Redistributions of source code must retain this license file.
- * 	b) Redistributions in binary form must reproduce this license file
- * 	in the documentation and/or other materials within the correspondent
- * 	distribution package.
- * 	c) Submitting back to Quorum Born IT all the modified versions of this
- * 	software source code.
- * 	d) Never to modify any binary forms of this software.
- * 	e) Unless it is Quorum Born IT incorporating received source code
- * 	modifications of this software in its base version, followed it with
- * 	the associated release, there is an explicit renounce to any Quorum
- * 	Born IT liability regarding the proper functioning of this software
- * 	and/or other software with which it interacts and/or integrates,
- * 	when modified by entities other than Quorum Born IT.
- *
- * 7. Activities involving the manipulation of this software not described in
- * this license, being in source code and/or binary forms, are outside its
- * scope and therefore are not allowed.
- *
- * 8. This software may not be error free, being licensed "as is" and
- * Quorum Born IT gives no express warranties, guarantees or conditions.
- * 	a) To the extent permitted under applicable laws, Quorum Born IT
- * 	excludes all implied warranties, including merchantability, fitness
- * 	for a particular purpose, and non-infringement.
- * 	b) Quorum Born IT shall not be liable for any lost profits, lost
- * 	revenues, lost opportunities, downtime, or any consequential damages
- * 	or costs, resulting from any claim or cause of action based on breach
- * 	of warranty, breach of contract, negligence, or any other legal theory,
- * 	that arises from the use of this software by others.
- *
- * 9. Everyone is permitted to copy and distribute verbatim copies of this
- * license document, but changes to it are not allowed.
- *
- *
- * Last updated: 13 July 2021
- */
 package com.qubit.qubEdu.module.candidacies.ui.customer.forwardpayments;
 
 import static org.fenixedu.treasury.util.TreasuryConstants.treasuryBundle;
@@ -118,28 +47,29 @@ public class CandidaciesCustomerAccountingForwardPaymentController
     }
 
     @Override
-    protected String redirectToDebtAccountUrl(final DebtAccount debtAccount, final Model model, final RedirectAttributes redirectAttributes) {
+    protected String redirectToDebtAccountUrl(final DebtAccount debtAccount, final Model model,
+            final RedirectAttributes redirectAttributes) {
         return redirect(readDebtAccountUrl() + activeDebtAccount(debtAccount).getExternalId(), model, redirectAttributes);
     }
 
     private DebtAccount activeDebtAccount(final DebtAccount debtAccount) {
-        return DebtAccount.findUnique(debtAccount.getFinantialInstitution(), ((PersonCustomer) debtAccount.getCustomer()).getActiveCustomer()).get();
+        return DebtAccount.findUnique(debtAccount.getFinantialInstitution(),
+                ((PersonCustomer) debtAccount.getCustomer()).getActiveCustomer()).get();
     }
 
     @RequestMapping(value = CHOOSE_INVOICE_ENTRIES_URI + "{debtAccountId}/{digitalPaymentPlatformId}")
     @Override
-    public String chooseInvoiceEntries(
-            @PathVariable(value = "debtAccountId") DebtAccount debtAccount,
+    public String chooseInvoiceEntries(@PathVariable(value = "debtAccountId") DebtAccount debtAccount,
             @PathVariable("digitalPaymentPlatformId") DigitalPaymentPlatform digitalPaymentPlatform,
-            @RequestParam(value = "bean", required = false) SettlementNoteBean bean, 
-            Model model, 
+            @RequestParam(value = "bean", required = false) SettlementNoteBean bean, Model model,
             final RedirectAttributes redirectAttributes) {
         return super.chooseInvoiceEntries(debtAccount, digitalPaymentPlatform, bean, model, redirectAttributes);
     }
 
     @RequestMapping(value = CHOOSE_INVOICE_ENTRIES_URI, method = RequestMethod.POST)
     @Override
-    public String chooseInvoiceEntries(@RequestParam(value = "bean", required = true) SettlementNoteBean bean, Model model, final RedirectAttributes redirectAttributes) {
+    public String chooseInvoiceEntries(@RequestParam(value = "bean", required = true) SettlementNoteBean bean, Model model,
+            final RedirectAttributes redirectAttributes) {
         return super.chooseInvoiceEntries(bean, model, redirectAttributes);
     }
 
@@ -192,8 +122,7 @@ public class CandidaciesCustomerAccountingForwardPaymentController
     public static final String FORWARD_PAYMENT_SUCCESS_URL = CONTROLLER_URL + FORWARD_PAYMENT_SUCCESS_URI;
 
     @RequestMapping(value = FORWARD_PAYMENT_SUCCESS_URI + "/{forwardPaymentId}", method = RequestMethod.GET)
-    public String forwardpaymentsuccess(@PathVariable("forwardPaymentId") ForwardPaymentRequest forwardPayment,
-            Model model) {
+    public String forwardpaymentsuccess(@PathVariable("forwardPaymentId") ForwardPaymentRequest forwardPayment, Model model) {
         return super.forwardpaymentsuccess(forwardPayment, model);
     }
 
@@ -201,8 +130,7 @@ public class CandidaciesCustomerAccountingForwardPaymentController
     public static final String FORWARD_PAYMENT_INSUCCESS_URL = CONTROLLER_URL + FORWARD_PAYMENT_INSUCCESS_URI;
 
     @RequestMapping(value = FORWARD_PAYMENT_INSUCCESS_URI + "/{forwardPaymentId}", method = RequestMethod.GET)
-    public String forwardpaymentinsuccess(@PathVariable("forwardPaymentId") ForwardPaymentRequest forwardPayment,
-            Model model) {
+    public String forwardpaymentinsuccess(@PathVariable("forwardPaymentId") ForwardPaymentRequest forwardPayment, Model model) {
         return super.forwardpaymentinsuccess(forwardPayment, model);
     }
 
@@ -221,5 +149,5 @@ public class CandidaciesCustomerAccountingForwardPaymentController
             RedirectAttributes redirectAttributes) {
         return super.printsettlementnote(settlementNote, model, redirectAttributes);
     }
-    
+
 }
