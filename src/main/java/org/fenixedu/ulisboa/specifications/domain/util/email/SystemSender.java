6ad4c370173892832328c829297bd2238c82df72
João Marques <joao.marques@qub-it.com>
2024-12-13 10:42:46 +0000

    Moves SystemSender to ULisboa because of specific emails sent in ULisboaServiceRequest Relates with: #qubIT-Omnis-7804

---
+++ src/main/java/org/fenixedu/ulisboa/specifications/domain/util/email/SystemSender.java
@@ -0,0 +1,20 @@
+package org.fenixedu.ulisboa.specifications.domain.util.email;
+
+import org.fenixedu.academic.domain.organizationalStructure.Unit;
+import org.fenixedu.academic.domain.util.email.Sender;
+import org.fenixedu.bennu.core.groups.Group;
+
+public class SystemSender extends SystemSender_Base {
+
+    public SystemSender() {
+        super();
+        setMembers(Group.managers());
+        setFromAddress(Sender.getNoreplyMail());
+        setSystemRootDomainObject(getRootDomainObject());
+        setFromName(createFromName());
+    }
+
+    public String createFromName() {
+        return String.format("%s (%s)", Unit.getInstitutionAcronym(), "Sistema Fénix");
+    }
+}
