81a3890cc5027f50e764175ab6382c6d3eb2890b
Sofia Nascimento <123949961+Sofia-Nascimento@users.noreply.github.com>
2025-06-12 12:22:54 +0100

    Refactor: remove StudentCandidacy from FirstYearRegistrationConfiguration (#47)
    Issues: #qubIT-Fenix-6559 #qubIT-Fenix-6696

--- src/main/java/org/fenixedu/ulisboa/specifications/domain/FirstYearRegistrationConfiguration.java
+++ src/main/java/org/fenixedu/ulisboa/specifications/domain/FirstYearRegistrationConfiguration.java
@@ -1,13 +1,8 @@
 package org.fenixedu.ulisboa.specifications.domain;
 
-import java.util.Comparator;
-import java.util.function.Predicate;
-
 import org.fenixedu.academic.domain.Degree;
 import org.fenixedu.academic.domain.DegreeCurricularPlan;
 import org.fenixedu.academic.domain.Person;
-import org.fenixedu.academic.domain.candidacy.CandidacySituationType;
-import org.fenixedu.academic.domain.candidacy.StudentCandidacy;
 import org.fenixedu.academic.domain.student.Registration;
 import org.fenixedu.academic.domain.student.Student;
 
@@ -39,29 +34,9 @@ public class FirstYearRegistrationConfiguration extends FirstYearRegistrationCon
     }
 
     public static boolean requiresVaccination(final Person person) {
-        StudentCandidacy candidacy = getCandidacy(person);
-        if (candidacy != null && requiresVaccination(candidacy.getDegreeCurricularPlan().getDegree())) {
-            return true;
-        }
-
         Student student = person.getStudent();
-        if (student != null) {
-            for (Registration registration : student.getRegistrationsSet()) {
-                if (requiresVaccination(registration.getDegree())) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private static StudentCandidacy getCandidacy(final Person person) {
-        final Predicate<StudentCandidacy> isCandidacyFirstTime = StudentCandidacy::getFirstTimeCandidacy;
-        final Predicate<StudentCandidacy> isCandidacyOpen = c -> CandidacySituationType.STAND_BY.equals(c.getState());
-
-        return person.getCandidaciesSet().stream().filter(c -> c instanceof StudentCandidacy).map(StudentCandidacy.class::cast)
-                .filter(isCandidacyFirstTime).filter(isCandidacyOpen).sorted(Comparator.comparing(StudentCandidacy::getStartDate))
-                .findFirst().orElse(null);
+        return student != null && student.getRegistrationsSet().stream().map(Registration::getDegree)
+                .anyMatch(d -> requiresVaccination(d));
     }
 
     private static boolean requiresVaccination(final Degree degree) {
