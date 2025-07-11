08cb56a44c0d31917b7220007acc5ce791589f47
Anil Mamede <anil.mamede@qub-it.com>
2025-07-11 14:42:37 +0100

    - Refactor: Remove references for CandidaciesExtensionsResources [#qubIT-Fenix-3172]

--- src/main/java/org/fenixedu/bennu/CandidaciesExtensionsConfiguration.java
+++ src/main/java/org/fenixedu/bennu/CandidaciesExtensionsConfiguration.java
@@ -2,9 +2,9 @@ package org.fenixedu.bennu;
 
 import org.fenixedu.bennu.spring.BennuSpringModule;
 
-@BennuSpringModule(basePackages = "com.qubit.qubEdu.module.candidacies", bundles = "CandidaciesExtensionsResources")
+@BennuSpringModule(basePackages = "com.qubit.qubEdu.module.candidacies", bundles = "FenixeduUlisboaSpecificationsResources")
 public class CandidaciesExtensionsConfiguration {
 
-    public static final String BUNDLE = "resources/CandidaciesExtensionsResources";
+    public static final String BUNDLE = "resources/FenixeduUlisboaSpecificationsResources";
 
 }
